f2209944ecf87fa9ef34d59813372147bc6177ac
Nick <37081556+naustica@users.noreply.github.com>
2019-06-14 23:27:54 +0200

    add dummy posts

--- nickipedia.py
+++ nickipedia.py
@@ -2,10 +2,43 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'naustica',
+        'username': 'naustica',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga vel, voluptates, doloremque nesciunt illum est corrupti nostrum expedita adipisci dicta vitae? Eveniet maxime quibusdam modi molestias alias et incidunt est.',
+    },
+    {
+        'author': 'naustica',
+        'username': 'naustica',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga vel, voluptates, doloremque nesciunt illum est corrupti nostrum expedita adipisci dicta vitae? Eveniet maxime quibusdam modi molestias alias et incidunt est.',
+    },
+    {
+        'author': 'naustica',
+        'username': 'naustica',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga vel, voluptates, doloremque nesciunt illum est corrupti nostrum expedita adipisci dicta vitae? Eveniet maxime quibusdam modi molestias alias et incidunt est.',
+    },
+    {
+        'author': 'naustica',
+        'username': 'naustica',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga vel, voluptates, doloremque nesciunt illum est corrupti nostrum expedita adipisci dicta vitae? Eveniet maxime quibusdam modi molestias alias et incidunt est.',
+    },
+    {
+        'author': 'naustica',
+        'username': 'naustica',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga vel, voluptates, doloremque nesciunt illum est corrupti nostrum expedita adipisci dicta vitae? Eveniet maxime quibusdam modi molestias alias et incidunt est.',
+    },
+    {
+        'author': 'naustica',
+        'username': 'naustica',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga vel, voluptates, doloremque nesciunt illum est corrupti nostrum expedita adipisci dicta vitae? Eveniet maxime quibusdam modi molestias alias et incidunt est.',
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
 @app.route('/user/<name>')
 def user(name):
