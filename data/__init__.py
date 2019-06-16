2c88b9c9716fbe349147c5c9e7478dd88d0b36bd
Nick <37081556+naustica@users.noreply.github.com>
2019-06-16 17:27:02 +0200

    small changes

--- data/__init__.py
+++ data/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_bcrypt import Bcrypt
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = "1und1"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-from data import routes
+from data import views
--- data/forms.py
+++ data/forms.py
@@ -1,4 +1,26 @@
-from wtforms import Form, StringField, SelectField
+from wtforms import Form, StringField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length
+from flask import flash
+from data.models import User
 
 class Search(Form):
     search = StringField('')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min=2, max=64)])
+
+    submit = SubmitField('sign up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            flash('get rekt username taken')
+            raise ValueError('username taken')
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min=2, max=64)])
+
+    submit = SubmitField('login')
