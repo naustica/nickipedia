0602f8a1e72f701c51e6321ffe0b661298a743b3
Nick <37081556+naustica@users.noreply.github.com>
2019-06-17 23:00:40 +0200

    fix searchform

--- data/forms.py
+++ data/forms.py
@@ -4,8 +4,18 @@ from wtforms.validators import DataRequired, Length
 from flask import flash
 from data.models import User
 
-class Search(Form):
-    search = StringField('')
+class SearchForm(FlaskForm):
+    search = StringField('search', validators=[DataRequired(), Length(min=2, max=64)])
+
+    submit = SubmitField('search')
+
+    def validate_search(self, search):
+
+        search = User.query.filter_by(username=search.data).first()
+
+        if search is None:
+            flash('get rekt no results')
+            raise ValueError('no results')
 
 class RegistrationForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(min=2, max=64)])
@@ -24,3 +34,11 @@ class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(min=2, max=64)])
 
     submit = SubmitField('login')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+
+        if user is None:
+            flash('get rekt username dont exist')
+            raise ValueError('username dont exist')
