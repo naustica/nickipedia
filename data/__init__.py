96d8fe8c3fac2c58eaca32c74ffe2603634cb2de
Nick <37081556+naustica@users.noreply.github.com>
2019-06-23 19:00:37 +0200

    add flask-dropzone app.config

--- data/__init__.py
+++ data/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_dropzone import Dropzone
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,12 +12,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "1und1"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'video' and 'image'
+app.config['DROPZONE_MAX_FILES'] = 1
+app.config['DROPZONE_TIMEOUT'] = 10000
+app.config['DROPZONE_DEFAULT_MESSAGE'] = 'drop files here to upload'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+dropzone = Dropzone(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
 
 from data import views
