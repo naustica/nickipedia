1a76a2b87240b703e10e14515658d5ede77a7fe9
Nick <37081556+naustica@users.noreply.github.com>
2019-06-23 18:58:11 +0200

    exclude uploader into utils

--- data/views.py
+++ data/views.py
@@ -1,10 +1,9 @@
 from data import app, login_manager
 from flask_login import login_user, logout_user, login_required
 from flask import render_template, request, flash, redirect, url_for
-from werkzeug import secure_filename
 from data.models import User, Post
 from data.models import db
-from data.utils import add_search, add_login, add_register, add_compose_poop
+from data.utils import add_search, add_login, add_register, add_compose_poop, add_uploader
 from sqlalchemy import desc
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,6 +20,7 @@ def login():
 @login_required
 @add_search
 @add_compose_poop
+@add_uploader
 def index():
     posts = Post.query.order_by(desc(Post.id)).all()
 
@@ -33,6 +33,7 @@ def index():
 @login_required
 @add_search
 @add_compose_poop
+@add_uploader
 def user(name):
 
     from data.utils import searchform, poopform
@@ -47,25 +48,6 @@ def register():
 
     return render_template('register.html', title='Register', registrationform=registrationform)
 
-@app.route('/upload', methods = ['GET', 'POST'])
-@login_required
-@add_search
-def upload():
-
-    from data.utils import searchform
-
-    return render_template('upload.html', searchform=searchform)
-
-@app.route('/uploader', methods = ['GET', 'POST'])
-@login_required
-def upload_file():
-    if request.method == 'POST':
-      f = request.files['file']
-      f.save('uploads/' + secure_filename(f.filename))
-      success = True
-      return render_template('upload.html', success=success)
-
-
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
@@ -74,13 +56,13 @@ def page_not_found(error):
 def unauthorized_callback():
     return redirect(url_for('login'))
 
-@app.route("/logout")
+@app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route("/reset")
+@app.route('/reset')
 def reset():
     db.drop_all()
     db.create_all()
