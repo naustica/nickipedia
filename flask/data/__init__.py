35d3de1d0a0608460d44f11368365d25aca13d4b
Nick <37081556+naustica@users.noreply.github.com>
2019-07-02 12:55:42 +0200

    change Database URI, Postgres support

--- flask/data/__init__.py
+++ flask/data/__init__.py
@@ -4,20 +4,25 @@ import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_dropzone import Dropzone
+from . import config
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "1und1"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'video' and 'image'
 app.config['DROPZONE_MAX_FILES'] = 1
 app.config['DROPZONE_TIMEOUT'] = 10000
 app.config['DROPZONE_DEFAULT_MESSAGE'] = 'drop files here to upload'
 
-db = SQLAlchemy(app)
+app.app_context().push()
+
+db = SQLAlchemy()
+db.init_app(app)
+db.create_all()
 bcrypt = Bcrypt(app)
 dropzone = Dropzone(app)
 
