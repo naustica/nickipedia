e982f668a656a13c52994aa103da397a5742b9e4
Nick <37081556+naustica@users.noreply.github.com>
2019-07-16 18:17:06 +0200

    add video view

--- flask/data/views.py
+++ flask/data/views.py
@@ -1,10 +1,11 @@
 from data import app, login_manager
 from flask_login import login_user, logout_user, login_required
 from flask import render_template, request, flash, redirect, url_for
-from data.models import User, Post
+from data.models import User, Comment, Video
 from data.models import db
-from data.utils import add_search, add_login, add_register, add_compose_poop, add_uploader
+from data.utils import add_search, add_login, add_register, add_comment_composer, add_uploader
 from sqlalchemy import desc
+
 
 @app.route('/login', methods=['GET', 'POST'])
 @add_login
@@ -20,25 +21,30 @@ def login():
 @login_required
 @add_search
 def index():
-    posts = Post.query.order_by(desc(Post.id)).all()
 
     from data.utils import searchform
 
-    return render_template('search.html', posts=posts, searchform=searchform)
+    current_page = 'index'
+
+    return render_template('index.html', searchform=searchform, current_page=current_page)
 
 
 @app.route('/user/<name>', methods=['GET', 'POST'])
 @login_required
 @add_search
-@add_compose_poop
+@add_comment_composer
 @add_uploader
 def user(name):
 
-    from data.utils import searchform, poopform
+    from data.utils import searchform, commentform
 
-    return render_template('user.html', name=name, searchform=searchform, poopform=poopform)
+    current_page = 'user'
 
-@app.route('/register', methods = ['GET', 'POST'])
+    return render_template('user.html', name=name, searchform=searchform,
+                            commentform=commentform, current_page=current_page)
+
+
+@app.route('/register', methods=['GET', 'POST'])
 @add_register
 def register():
 
@@ -46,19 +52,60 @@ def register():
 
     return render_template('register.html', title='Register', registrationform=registrationform)
 
+
+@app.route('/watch/<video_id>', methods=['GET', 'POST'])
+@add_comment_composer
+def video(video_id):
+
+    current_page = 'video'
+
+    video = Video.query.filter_by(id=video_id).first()
+
+    if video is None:
+        return redirect(404)
+
+    root = video.root
+
+    video_title = video.title
+
+    video_author = User.query.filter_by(id=video.author_id).first()
+
+    video_description = video.text
+
+    comments = Comment.query.filter_by(video_id=video_id).order_by(desc(Comment.id)).all()
+
+    from data.utils import commentform
+
+    return render_template('watch.html', video_id=video_id, comments=comments,
+                            commentform=commentform, root=root, video_title=video_title,
+                            video_description=video_description, video_author=video_author,
+                            current_page=current_page)
+
+
+@app.route('/result/<search>', methods=['GET', 'POST'])
+def result(search):
+
+    current_page = 'result'
+
+    return render_template('results.html', search=search, current_page=current_page)
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
 
+
 @login_manager.unauthorized_handler
 def unauthorized_callback():
     return redirect(url_for('login'))
+
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
 
 @app.route('/reset')
 def reset():
