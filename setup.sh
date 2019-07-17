0ef971e040931d9425ba207599bee7ecd9ce1981
Nick <37081556+naustica@users.noreply.github.com>
2019-07-17 17:01:27 +0200

    add paths

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-./node_modules/.bin/webpack
-./flask/data/static/js
-tsc main.ts
+readonly BASENAME=$PWD
+
+#cd ./flask/data/static/js
+#tsc main.ts
+#cd ~
+#cd "$BASENAME"
+
+
+#cd ./node_modules/.bin/webpack
+#cd ~
+#cd "$BASENAME"
+
+python ./flask/dbconfig.py
