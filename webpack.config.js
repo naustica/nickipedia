97d05d318c9967e9b9967b67338fce337d60a6b8
Nick <37081556+naustica@users.noreply.github.com>
2019-07-18 21:58:00 +0200

    ready for files with .tsx extension

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-  mode: 'production',
-  entry: ['./flask/data/static/js/main.ts'],
+  mode: "production",
+  devtool: "source-map",
+  entry: ["./flask/data/static/js/main.tsx"],
   output: {
     path: path.resolve(__dirname, "flask/data/static/js"),
-    filename: 'main.js'
+    filename: "main.js"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts"]
   },
   module: {
     rules: [
-      { test: /\.ts$/, use: 'ts-loader' }
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: "ts-loader"
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
     ]
-  }
+  },
+
+  externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
 };
