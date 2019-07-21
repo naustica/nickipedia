f33cd687b686ec52cb57e608111cc4181ea82762
Nick <37081556+naustica@users.noreply.github.com>
2019-07-21 15:23:21 +0200

    add routes to api

--- flask/data/api/routes.py
+++ flask/data/api/routes.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, jsonify
-from data.models import Video
+from flask import Blueprint, jsonify, request
+from data.database.video import Video
+from data.database.user import User
 
 
 bp = Blueprint('routes', __name__, url_prefix='/api')
 
 
 @bp.route('/search/<term>', methods=['GET'])
-def get(term):
+def get_results(term):
     search = Video.query.filter(Video.title.like('{}{}{}'.format('%', term, '%'))).all()
 
     data = []
@@ -14,5 +15,26 @@ def get(term):
     for result in search:
         result = {'id': int(result.id), 'properties': {'title': str(result.title), 'text': str(result.text)}}
         data.append(result)
+
+    return jsonify(data)
+
+
+@bp.route('/user', methods=['GET'])
+def get_users():
+    users = User.get_all()
+
+    data = []
+
+    for user in users:
+        result = {'id': int(user.id), 'properties': {'username': user.username}}
+        data.append(result)
+
+    return jsonify(data)
+
+
+@bp.route('/user/<user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.filter(User.id == user_id).first()
+    data = {'id': int(user.id), 'properties': {'username': user.username}}
 
     return jsonify(data)
