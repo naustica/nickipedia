1679095a48e73fb66d7b1fca59d5dadadc5d3bcf
Nick <37081556+naustica@users.noreply.github.com>
2019-07-23 12:22:51 +0200

    splitting routes

--- flask/data/api/routes.py
+++ flask/data/api/routes.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from data.database.video import Video
-from data.database.user import User
 
 
 bp = Blueprint('routes', __name__, url_prefix='/api')
@@ -8,33 +7,12 @@ bp = Blueprint('routes', __name__, url_prefix='/api')
 
 @bp.route('/search/<term>', methods=['GET'])
 def get_results(term):
-    search = Video.query.filter(Video.title.like('{}{}{}'.format('%', term, '%'))).all()
+    search = Video.query.filter(Video.title.ilike('{}{}{}'.format('%', term, '%'))).all()
 
     data = []
 
     for result in search:
         result = {'id': int(result.id), 'properties': {'title': str(result.title), 'text': str(result.text)}}
         data.append(result)
-
-    return jsonify(data)
-
-
-@bp.route('/user', methods=['GET'])
-def get_users():
-    users = User.get_all()
-
-    data = []
-
-    for user in users:
-        result = {'id': int(user.id), 'properties': {'username': user.username}}
-        data.append(result)
-
-    return jsonify(data)
-
-
-@bp.route('/user/<user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.filter(User.id == user_id).first()
-    data = {'id': int(user.id), 'properties': {'username': user.username}}
 
     return jsonify(data)
