abf7b894db1c0dc18aa4613f8e8413b68f5c4f38
Nick <37081556+naustica@users.noreply.github.com>
2019-07-23 12:21:21 +0200

    delete unnecessary module imports

--- flask/data/website/forms.py
+++ flask/data/website/forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, SubmitField, TextAreaField, TextField
+from wtforms import StringField, SubmitField, TextField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Length
 from flask import flash
