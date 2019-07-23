04b9b99ccc0113e1159d57df36767b5d84e40e5d
Nick <37081556+naustica@users.noreply.github.com>
2019-07-23 12:21:44 +0200

    add authentification ability

--- flask/data/database/user.py
+++ flask/data/database/user.py
@@ -1,21 +1,21 @@
-from data import db, login_manager
-from flask_login import UserMixin
+from data import db, ma, login_manager
 
 
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-
-class User(db.Model, UserMixin):
+class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=False, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    authenticated = db.Column(db.Boolean, default=False)
     comments = db.relationship('Comment', backref='author', lazy=True)
     videos = db.relationship('Video', backref='owner', lazy=True)
 
-    def __init__(self, username):
+    def __init__(self, username, email, password, authenticated=False):
         self.username = username
+        self.email = email
+        self.password = password
+        self.authenticated = authenticated
 
     def save(self):
         db.session.add(self)
@@ -25,9 +25,35 @@ class User(db.Model, UserMixin):
         db.session.delete(self)
         db.session.commit()
 
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.id
+
+    def is_authenticated(self):
+        self.authenticated
+
+    def is_anonymous(self):
+        return False
+
     @staticmethod
     def get_all():
         return User.query.all()
 
     def __repr__(self):
         return '{}'.format(self.username)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('username', 'email')
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
