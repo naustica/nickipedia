8698696f63e34ae965c295370299fd79ea002469
Nick <37081556+naustica@users.noreply.github.com>
2019-07-23 12:22:31 +0200

    setting up marshmallow and jwt extended

--- flask/data/__init__.py
+++ flask/data/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from flask_dropzone import Dropzone
 from flask_cors import CORS
+from flask_marshmallow import Marshmallow
+from flask_login import LoginManager
+from flask_jwt_extended import JWTManager
 import os
 
 
 db = SQLAlchemy()
+ma = Marshmallow()
 bcrypt = Bcrypt()
 dropzone = Dropzone()
 login_manager = LoginManager()
+jwt = JWTManager()
 
 
 def create_app():
@@ -21,15 +25,23 @@ def create_app():
     app.config.from_object(os.environ['APP_SETTINGS'])
 
     db.init_app(app)
+    ma.init_app(app)
     bcrypt.init_app(app)
     dropzone.init_app(app)
+    jwt.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'views.login'
 
     from data.website import views
     app.register_blueprint(views.bp)
 
     from data.api import routes
     app.register_blueprint(routes.bp)
+
+    from data.api import user
+    app.register_blueprint(user.bp)
+
+    from data.api import auth
+    app.register_blueprint(auth.bp)
 
     return app
