e458fee93433f0ae3ad00568e0ff19d1a3ac9371
Nick <37081556+naustica@users.noreply.github.com>
2019-07-23 18:16:23 +0200

    add routes to comment api

--- flask/data/api/comment.py
+++ flask/data/api/comment.py
@@ -5,6 +5,26 @@ from data.database.comment import Comment
 bp = Blueprint('comment', __name__, url_prefix='/api')
 
 
-@bp.route('/comment', methods=['POST'])
-def create_comment():
+@bp.route('/comment/<video_id>', methods=['POST'])
+def create_comment(id):
+    return True, 200
+
+
+@bp.route('/comment/<id>', methods=['GET'])
+def get_all_comments(id):
+    return True, 200
+
+
+@bp.route('/comment/<id>/<comment_id>', methods=['PUT'])
+def update_comment(id, comment_id):
+    return True, 200
+
+
+@bp.route('/comment/<id>/<comment_id>', methods=['DELETE'])
+def delete_comment(id, comment_id):
+    return True, 200
+
+
+@bp.route('/comment/<id>/<comment_id>', methods=['GET'])
+def get_comment(id, comment_id):
     return True, 200
