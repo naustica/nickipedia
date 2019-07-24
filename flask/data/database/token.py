84c31dd49ad859b171f972094a8cde291df89fd0
Nick <37081556+naustica@users.noreply.github.com>
2019-07-24 12:19:55 +0200

    add token database schema

---
+++ flask/data/database/token.py
@@ -0,0 +1,24 @@
+from data import db
+from sqlalchemy import func
+
+
+class Token(db.Model):
+    __tablename__ = 'tokens'
+    token = db.Column(db.String(), primary_key=True, nullable=False)
+    username = db.Column(db.String(32), db.ForeignKey('users.username'), nullable=False)
+    activated = db.Column(db.DateTime(), server_default=func.now())
+
+    def __init__(self, token, username):
+        self.token = token
+        self.username = username
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '{}: {}'.format(self.token, self.username)
