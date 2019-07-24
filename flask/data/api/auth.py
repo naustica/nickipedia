7300b94367151dee800f584cdf7daef0d9c6e6a9
Nick <37081556+naustica@users.noreply.github.com>
2019-07-24 12:19:15 +0200

    token system works

--- flask/data/api/auth.py
+++ flask/data/api/auth.py
@@ -1,48 +1,41 @@
 from flask import Blueprint, jsonify, request, make_response
 from data.database.user import User
+from data.database.token import Token
 from flask_jwt_extended import create_access_token, decode_token
 from data import bcrypt, db
-from datetime import timedelta
+from functools import wraps
 
 
 bp = Blueprint('auth', __name__, url_prefix='/api/auth')
 
 
-@bp.route('/register', methods=['POST'])
-def register_user():
-
-    if not request.is_json:
-        return make_response(jsonify(message='missing json')), 400
-
-    username = request.json.get('username', None)
-    email = request.json.get('email', None)
-    password = request.json.get('password', None)
-
-    if not username:
-        return make_response(jsonify(message='missing username parameter')), 400
-
-    if not email:
-        return make_response(jsonify(message='missing email parameter')), 400
-
-    if not password:
-        return make_response(jsonify(message='missing password parameter')), 400
-
-    user = User.query.filter_by(username=username).first()
-
-    if not user:
+def permission_needed(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
         try:
-            new_user = User(username, email, password)
-            new_user.save()
-            new_user.authenticated = True
-            db.session.commit()
-            expires = timedelta(seconds=60)
-            access_token = create_access_token(identity=username, expires_delta=expires)
-            return make_response(jsonify(status='success', access_token=access_token)), 200
+            access_token = request.headers.get('Authorization')
+
+            if not access_token:
+                return make_response(jsonify(message='missing access_token'))
+
+            decoded_token = decode_token(access_token)
+
+            username = decoded_token['identity']
+
+            user = User.query.get(username)
+
+            token = Token.query.filter_by(token=access_token, username=user.username)
+
+            if not user:
+                return make_response(jsonify(message='user not found')), 400
+
+            if not token:
+                return make_response(jsonify(message='invalid token'))
 
         except Exception:
-            return make_response(jsonify(status='fail', message='error. please try again.')), 400
-    else:
-        return make_response(jsonify(status='fail', message='user already exists.')), 400
+            return make_response(jsonify(message='permission needed.')), 400
+        return function(*args, **kwargs)
+    return wrapper
 
 
 @bp.route('/login', methods=['POST'])
@@ -60,20 +53,45 @@ def login():
     if not password:
         return make_response(jsonify(message='missing password parameter')), 400
 
-    user = User.query.filter_by(username=username).first()
-
-    jwt_decoded = decode_token(password)
-    password = jwt_decoded['identity']
+    user = User.query.get(username)
 
     try:
         if bcrypt.check_password_hash(user.password, password):
             user.authenticated = True
             db.session.commit()
-            expires = timedelta(seconds=60)
-            access_token = create_access_token(identity=username, expires_delta=expires)
+            access_token = create_access_token(identity=username)
+            Token(access_token, user.username).save()
             return make_response(jsonify(status='success', access_token=access_token)), 200
         else:
-            return make_response(jsonify(status='fail', message='wrong keywords, maybe password not encoded.'))
+            return make_response(jsonify(status='fail', message='wrong keywords, maybe password not encoded.')), 400
     except Exception as e:
         print(e)
         return make_response(jsonify(status='fail', message='try again')), 500
+
+
+@bp.route('/logout', methods=['POST'])
+@permission_needed
+def logout():
+    access_token = request.headers.get('Authorization')
+
+    decoded_token = decode_token(access_token)
+
+    username = decoded_token['identity']
+
+    user = User.query.get(username)
+
+    if not user:
+        return make_response(jsonify(message='user not found')), 400
+
+    token = Token.query.get(access_token)
+
+    if token.username != user.username:
+        return make_response(jsonify(message='wrong token username combination'))
+
+    token.delete()
+
+    user.authenticated = False
+
+    db.session.commit()
+
+    return make_response(jsonify(message='successfully logged out.')), 200
