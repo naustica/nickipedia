698455dd9f718f6603e9524a8915dbe34d88c187
Nick <37081556+naustica@users.noreply.github.com>
2019-07-25 16:54:59 +0200

    add videos from url

--- flask/data/api/video.py
+++ flask/data/api/video.py
@@ -7,6 +7,8 @@ import os
 from data.config import upload_path
 from fnmatch import fnmatch
 from datetime import datetime
+import pafy
+from flask_jwt_extended import decode_token
 
 
 bp = Blueprint('video', __name__, url_prefix='/api')
@@ -15,6 +17,9 @@ bp = Blueprint('video', __name__, url_prefix='/api')
 @bp.route('/video', methods=['POST'])
 #@permission_needed
 def create_video():
+    """
+    example: POST: host/api/video
+    """
 
     title = request.form['title']
     text = request.form['text']
@@ -51,8 +56,46 @@ def create_video():
     return make_response(jsonify(message='video created')), 201
 
 
+@bp.route('/video/add_from_url', methods=['GET'])
+def add_video_from_url():
+
+    url = request.args.get('url', default='', type=str)
+
+    """
+    access_token = request.headers.get('Authorization')
+
+    decoded_token = decode_token(access_token)
+
+    author_id = decoded_token['identity']
+    """
+
+    author_id = 'admin'  # for test purpose
+
+    path = '{}{}/{}'.format(upload_path, 'videos', author_id)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    new_filename = secure_filename(str(datetime.now()) + '.mp4')
+
+    video = pafy.new(url)
+
+    title = video.title
+    text = video.description
+    best = video.getbest()
+
+    best.download(filepath='{}{}{}'.format(path, '/', new_filename))
+
+    new_video = Video(author_id, title, text, path, new_filename)
+    new_video.save()
+    return make_response(jsonify(message='video added')), 201
+
+
 @bp.route('/video', methods=['GET'])
 def get_videos():
+    """
+    example: GET: host/api/video/?video_id=1
+    """
 
     video_id = request.args.get('video_id', default=0, type=int)
     all = request.args.get('all', default=False, type=bool)
@@ -77,6 +120,9 @@ def get_videos():
 @bp.route('/video', methods=['PUT'])
 #@permission_needed
 def update_video():
+    """
+    example: PUT: host/api/video/?video_id=1
+    """
 
     if not request.is_json:
         return make_response(jsonify(message='missing json')), 400
@@ -107,6 +153,9 @@ def update_video():
 @bp.route('/video', methods=['DELETE'])
 #@permission_needed
 def delete_video():
+    """
+    example: DELETE: host/api/video/?video_id=1
+    """
 
     video_id = request.args.get('video_id', default=0, type=int)
 
