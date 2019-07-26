04a86f17f854ab5b0686d5a0ce9e68e41a9d10c4
Nick <37081556+naustica@users.noreply.github.com>
2019-07-26 11:26:21 +0200

    update user pic route

--- flask/data/api/user.py
+++ flask/data/api/user.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, jsonify, request, make_response
 from data.database.user import User, user_schema, users_schema
-from data import db
 from data.api.auth import permission_needed
+from werkzeug import secure_filename
+import os
+from data.config import upload_path
+from fnmatch import fnmatch
+from datetime import datetime
 
 
 bp = Blueprint('user', __name__, url_prefix='/api')
@@ -73,6 +77,43 @@ def user_update():
     user.update(**data)
 
     return make_response(jsonify(message='user updated')), 200
+
+
+@bp.route('/user/photo', methods=['PUT'])
+def user_pic_update():
+    """
+    example: PUT: host/api/user/photo?username=nickipedia
+    """
+
+    username = request.args.get('username', default='', type=str)
+
+    user = User.query.get(username)
+
+    if not user:
+        return make_response(jsonify(message='user not found')), 400
+
+    pic = request.files.get('file')
+
+    if not pic:
+        return make_response(jsonify(message='no file found.')), 400
+
+    filename = secure_filename(pic.filename)
+
+    if not fnmatch(filename, '*.jpg'):
+        return make_response(jsonify(message='the video format should be jpg.')), 400
+
+    path = '{}{}/{}'.format(upload_path, 'videos', user.username)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    new_filename = secure_filename(str(datetime.now()) + '.jpg')
+    path = '{}{}{}'.format(path, '/', new_filename)
+    pic.save(path)
+
+    user.profile_picure = path
+    user.save()
+    return make_response(jsonify(message='profile picure updated.')), 201
 
 
 @bp.route('/user', methods=['DELETE'])
