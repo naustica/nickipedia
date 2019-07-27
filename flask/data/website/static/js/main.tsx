75aba173ea0f0d5dcd3d343cde90fe28f9021e29
Nick <37081556+naustica@users.noreply.github.com>
2019-07-27 11:45:55 +0200

    change import

--- flask/data/website/static/js/main.tsx
+++ flask/data/website/static/js/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Put from 'react-axios';
+import get from 'axios';
+
 
 import SearchResult from './results';
 
 // ReactDOM.render(<SearchResult/>, document.getElementById('root'));
- 
