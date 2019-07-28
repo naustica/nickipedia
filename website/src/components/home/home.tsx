4f1eee7c8473a50ecc7a39408624b6c8192c2376
Nick <37081556+naustica@users.noreply.github.com>
2019-07-28 23:05:14 +0200

    add typescript types

--- website/src/components/home/home.tsx
+++ website/src/components/home/home.tsx
@@ -4,7 +4,7 @@ import './home.css'
 
 
 class Home extends Component<{}, { term: string }> {
-  constructor(props) {
+  constructor(props:never) {
     super(props)
     this.state = {
       term: ''
@@ -12,7 +12,7 @@ class Home extends Component<{}, { term: string }> {
     this.changeTerm = this.changeTerm.bind(this)
     this.submitForm = this.submitForm.bind(this)
   }
-  changeTerm(event) {
+  changeTerm(event:React.ChangeEvent<HTMLInputElement>): void {
     this.setState({term: event.target.value})
   }
   submitForm() {
@@ -21,7 +21,7 @@ class Home extends Component<{}, { term: string }> {
   render() {
     return (
       <div className="container" id="searchform-container">
-        <form method="#" onSubmit={this.submitForm}>
+        <form onSubmit={this.submitForm}>
           <input id="searchform-textarea" type="text" name="search" onChange={this.changeTerm} autoFocus placeholder="search..."/>
         </form>
       </div>
