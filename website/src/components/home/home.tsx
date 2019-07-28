b99b2442b74309299e60bb4b12de5e464caeeea8
Nick <37081556+naustica@users.noreply.github.com>
2019-07-28 19:22:39 +0200

    add search functionality

--- website/src/components/home/home.tsx
+++ website/src/components/home/home.tsx
@@ -1,3 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './home.css'
+
+
+class Home extends Component<{}, { term: string }> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      term: ''
+    }
+    this.changeTerm = this.changeTerm.bind(this)
+    this.submitForm = this.submitForm.bind(this)
+  }
+  changeTerm(event) {
+    this.setState({term: event.target.value})
+  }
+  submitForm() {
+    console.log(this.state.term)
+  }
+  render() {
+    return (
+      <div className="container" id="searchform-container">
+        <form method="#" onSubmit={this.submitForm}>
+          <input id="searchform-textarea" type="text" name="search" onChange={this.changeTerm} autoFocus placeholder="search..."/>
+        </form>
+      </div>
+  )
+  }
+}
+
+
+export default Home;
