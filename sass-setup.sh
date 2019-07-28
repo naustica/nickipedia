c07d5105eeca11f9fcddba0e1816770bd6788af4
Nick <37081556+naustica@users.noreply.github.com>
2019-07-28 16:43:16 +0200

    change path

--- sass-setup.sh
+++ sass-setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cd ./flask/data/website/static/css/
-sass --watch main.scss:main.css
+#cd ./flask/data/website/static/css/
+#sass --watch main.scss:main.css
+
+cd ./website
+sass --watch .
