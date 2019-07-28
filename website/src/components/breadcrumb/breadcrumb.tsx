578e839d2efe8cae9cba7722f115330b9d27edf5
Nick <37081556+naustica@users.noreply.github.com>
2019-07-28 16:44:47 +0200

    add breadcrumb component

---
+++ website/src/components/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+import './breadcrumb.css';
+
+
+class Breadcrumb extends Component {
+
+    render() {
+      return (
+          <nav className="nav-breadcrumb" aria-label="breadcrumb">
+            <ol className="breadcrumb" id="ol-breadcrumb">
+              <li className="breadcrumb-item"><Link to='/'>home</Link></li>
+              <li className="breadcrumb-item active" aria-current="page">current page</li>
+            </ol>
+          </nav>
+      )
+    }
+
+}
+
+
+export default Breadcrumb
