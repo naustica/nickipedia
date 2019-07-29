78a3ec2139d70529e47d172960c2694a5bb19c3e
Nick <37081556+naustica@users.noreply.github.com>
2019-07-29 17:31:26 +0200

    add api call

--- website/src/components/home/home.tsx
+++ website/src/components/home/home.tsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 import './home.scss'
 
@@ -9,20 +10,32 @@ class Home extends Component<{}, { term: string }> {
     this.state = {
       term: ''
     }
-    this.changeTerm = this.changeTerm.bind(this)
+    this.getTerm = this.getTerm.bind(this)
     this.submitForm = this.submitForm.bind(this)
   }
-  changeTerm(event:React.ChangeEvent<HTMLInputElement>): void {
+  getTerm(event:React.ChangeEvent<HTMLInputElement>): void {
     this.setState({term: event.target.value})
   }
-  submitForm() {
-    console.log(this.state.term)
+  submitForm(event:React.FormEvent<HTMLFormElement>): void {
+    var form = event.target as HTMLFormElement;
+    event.preventDefault();
+    axios.get('api/search?term=' + this.state.term)
+      .then(response => {
+        const results = response.data;
+        console.log(results);
+      })
+      .catch(error => {
+        console.log(error)
+        form.reset();
+      })
   }
   render() {
     return (
       <div className="container" id="searchform-container">
-        <form onSubmit={this.submitForm}>
-          <input id="searchform-textarea" type="text" name="search" onChange={this.changeTerm} autoFocus placeholder="search..."/>
+        <form method="POST" onSubmit={this.submitForm}>
+          <div className="form-group input-group-lg">
+            <input className="form-control from-control-lg" type="text" name="search" onChange={this.getTerm} autoFocus placeholder="search..."/>
+          </div>
         </form>
       </div>
   )
