24d2ee00083db9446c94cc098c772f71ab955bd7
Nick <37081556+naustica@users.noreply.github.com>
2019-07-30 15:23:06 +0200

    import authentication component

--- website/src/app.tsx
+++ website/src/app.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, HashRouter, Route, Switch, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import './app.scss';
 
@@ -9,6 +9,8 @@ import Home from './components/home/home';
 import Login from './components/login/login';
 import Register from './components/register/register';
 import Results from './components/results/results';
+import Logout from './components/logout/logout';
+import AuthenticatedRoute from './components/authentication/authentication';
 
 
 function Settings() {
@@ -35,12 +37,13 @@ class App extends Component {
                           <div className="col">
                               <div>
                                 <Switch>
-                                  <Route path='/' exact component={Home}/>
+                                  <AuthenticatedRoute path='/' exact component={Home}/>
                                   <Route path='/home' exact component={Home}/>
                                   <Route path='/settings' component={Settings}/>
                                   <Route path='/about' component={About}/>
                                   <Route path='/login' component={Login}/>
                                   <Route path='/register' component={Register}/>
+                                  <AuthenticatedRoute path='/logout' component={Logout}/>
                                   <Route path='/result:term' component={Results}/>
                                   <Route component={Error}/>
                                 </Switch>
