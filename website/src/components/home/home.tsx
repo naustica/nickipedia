c46cd0453dee58da9ef4992486dd022edeb10006
Nick <37081556+naustica@users.noreply.github.com>
2019-07-30 11:27:57 +0200

    delete unnecessary module imports

--- website/src/components/home/home.tsx
+++ website/src/components/home/home.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import {Redirect, withRouter} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
 import './home.scss'
 
