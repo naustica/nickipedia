a93069951f080bd227ad84e7801cf94a9f9a9a0f
Nick <37081556+naustica@users.noreply.github.com>
2019-08-01 16:15:08 +0200

    input validation and some styling

--- website/src/components/register/register.tsx
+++ website/src/components/register/register.tsx
@@ -4,14 +4,18 @@ import {withRouter, Link} from 'react-router-dom';
 
 import './register.scss'
 
+import Loading from './../loading/loading';
 
-class Register extends Component<{history:any}, { username?: string, password?: string, email?: string }> {
+
+class Register extends Component<{history:any}, { username?: string, password?: string, email?: string, loading?: boolean, error?: string }> {
   constructor(props:any) {
     super(props)
     this.state = {
       username: '',
-      email: 'test@kek.de',
-      password: 'test'
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
     }
     this.onChange = this.onChange.bind(this)
     this.submitForm = this.submitForm.bind(this)
@@ -19,32 +23,63 @@ class Register extends Component<{history:any}, { username?: string, password?: 
   onChange(event:React.ChangeEvent<HTMLInputElement>): void {
     this.setState({[event.target.name]: event.target.value})
   }
+  validateForm(): boolean {
+    if (this.state.username === '' || this.state.password === '' || this.state.email === '') {
+      this.setState({error: '*username, email and password required'})
+      return false
+    }
+    return true
+  }
   submitForm(event:React.FormEvent<HTMLFormElement>): any {
     var form = event.target as HTMLFormElement;
     event.preventDefault();
-    fetch('api/user/register', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({username: this.state.username, email: this.state.email, password: this.state.password})
-    })
-      .then(() => {
-        this.props.history.push('/login')
+    if (this.validateForm()) {
+      fetch('api/user/register', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username: this.state.username, email: this.state.email, password: this.state.password})
       })
-      .catch(error => {
-        console.log(error)
-        form.reset();
-      })
+      .then((response) => response.json())
+        .then((data) => {
+          if (data.status != 'success') {
+            this.setState({loading: false, error: '*username or email exists'})
+            form.reset();
+          } else {
+            this.props.history.push('/login')
+          }
+        })
+        .catch(error => {
+          console.log(error)
+          form.reset();
+        })
+    }
   }
   render() {
     return (
       <div className="container" id="register-container">
-        <form onSubmit={this.submitForm}>
-          <div className="form-group input-group-lg">
-            <input className="form-control from-control-lg" type="text" name="username" autoFocus onChange={this.onChange} placeholder="username"/>
+        <div className="card" style={{padding: "1.5rem", opacity: 0.95}}>
+          <div className="card-body">
+            <h5 className="card-title" style={{textAlign: "center", padding: "2rem"}}>registration</h5>
+            <div style={{textAlign: "center", color: "red", padding: "1rem"}}>
+              {this.state.error}
+            </div>
+            <form onSubmit={this.submitForm}>
+              <div className="form-group input-group-lg" style={{padding: "0.8rem"}}>
+                <input className="form-control from-control-lg" type="text" name="username" autoFocus value={this.state.username} onChange={this.onChange} placeholder="username"/>
+              </div>
+              <div className="form-group input-group-lg" style={{padding: "0.8rem"}}>
+                <input className="form-control from-control-lg" type="email" name="email" value={this.state.email} onChange={this.onChange} placeholder="email"/>
+              </div>
+              <div className="form-group input-group-lg" style={{padding: "0.8rem"}}>
+                <input className="form-control from-control-lg" type="password" name="password" value={this.state.password} onChange={this.onChange} placeholder="password"/>
+              </div>
+              <div className="form-group input-group-lg" style={{textAlign: "center", padding: "2rem"}}>
+                <Link to="/login" style={{fontSize: "16px"}}>already have an account?</Link>
+              </div>
+              <button type="submit" style={{display: "none"}}></button>
+              <Loading loading={this.state.loading}/>
+            </form>
           </div>
-        </form>
-        <div className="register-info">
-          <Link to="/login" style={{fontSize: "16px"}}>already have an account?</Link>
         </div>
       </div>
   )
