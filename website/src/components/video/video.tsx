3320a5923a1e7c6a85536c4a0e6c83d701673054
Nick <37081556+naustica@users.noreply.github.com>
2019-08-02 12:05:17 +0200

    polishing

--- website/src/components/video/video.tsx
+++ website/src/components/video/video.tsx
@@ -4,63 +4,48 @@ import './video.scss'
 
 import VideoStream from './stream/stream';
 import VideoDescription from './description/description';
-import VideoPostComments from './comments/postComments';
-import VideoGetComments from './comments/getComments';
+import VideoComments from './comments/comments';
 import VideoSuggestions from './suggestions/suggestions';
 
 
-class Video extends Component<{match?: any}, {title: string, description: string, author: string, getCommentsComponent: any}> {
+class Video extends Component<{match?: any}, {title: string, description: string, author: string}> {
   constructor(props:any) {
     super(props)
     this.state = {
       title: '',
       description: '',
-      author: '',
-      getCommentsComponent: null
+      author: ''
     }
   }
-  componentDidMount() {
+  async componentDidMount() {
     const {id} = this.props.match.params
-    fetch('api/video?video_id=' + id, {
+    await fetch('api/video?video_id=' + id, {
       method: 'get'
     })
     .then((response) => response.json())
     .then((data) => {
       this.setState({title: data.title, description: data.text, author: data.author_id})
-      fetch('api/comment?all=True&video_id=' + id, {
-        method: 'get'
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({getCommentsComponent: data.map(comment => <VideoGetComments key={comment.id} comment={comment} />)})
-      })
-      .catch(() => {
-        this.setState({getCommentsComponent: <div></div>})
-      })
-
     })
     .catch(error => {
       console.log(error)
     })
   }
   render() {
+    const {id} = this.props.match.params
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-9">
             <VideoStream />
             <VideoDescription title={this.state.title} description={this.state.description} author={this.state.author}/>
-            <div className="container">
-              <VideoPostComments />
-            </div>
           </div>
           <div className="col-sm-3" style={{backgroundColor: "transparent", opacity: 0.95}}>
-            <VideoSuggestions />
+            <VideoSuggestions id={id}/>
           </div>
         </div>
         <div className="row">
           <div className="container">
-            {this.state.getCommentsComponent}
+            <VideoComments id={id}/>
           </div>
         </div>
       </div>
