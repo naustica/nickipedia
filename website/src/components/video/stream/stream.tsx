88654bcc42007b301e1e678c0336483f4c5256cd
Nick <37081556+naustica@users.noreply.github.com>
2019-08-02 13:47:13 +0200

    add reading props

--- website/src/components/video/stream/stream.tsx
+++ website/src/components/video/stream/stream.tsx
@@ -3,13 +3,13 @@ import React, {Component} from 'react';
 import './../video.scss'
 
 
-class VideoStream extends Component<{}, {}> {
+class VideoStream extends Component<{author: string, filename: string}, {}> {
   constructor(props:any) {
     super(props)
   }
   render() {
     return (
-      <video className="card-img-top" poster="http://0.0.0.0:8000/videos/admin/test.mp4" id="player" style={{width:"100%", borderRadius: "5px", outline: "none"}} playsInline controls>
+      <video className="card-img-top" poster={"http://0.0.0.0:8000/videos/admin/test.mp4"} id="player" style={{width:"100%", borderRadius: "5px", outline: "none"}} playsInline controls>
         <source src="http://0.0.0.0:8000/videos/admin/test.mp4" type="video/mp4" />
       </video>
     )
