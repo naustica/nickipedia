123bc550851e2820cfb472f1695a6380449d6964
Nick <37081556+naustica@users.noreply.github.com>
2019-08-02 12:03:57 +0200

    add source for poster

--- website/src/components/video/stream/stream.tsx
+++ website/src/components/video/stream/stream.tsx
@@ -9,7 +9,7 @@ class VideoStream extends Component<{}, {}> {
   }
   render() {
     return (
-      <video className="card-img-top" poster="" id="player" style={{width:"100%", borderRadius: "5px", outline: "none"}} playsInline controls>
+      <video className="card-img-top" poster="http://0.0.0.0:8000/videos/admin/test.mp4" id="player" style={{width:"100%", borderRadius: "5px", outline: "none"}} playsInline controls>
         <source src="http://0.0.0.0:8000/videos/admin/test.mp4" type="video/mp4" />
       </video>
     )
