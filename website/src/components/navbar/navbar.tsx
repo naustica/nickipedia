4a9f224fbe09c74786e033c98e9cbcc79adc555f
Nick <37081556+naustica@users.noreply.github.com>
2019-08-03 16:45:46 +0200

    add icons to navbar

--- website/src/components/navbar/navbar.tsx
+++ website/src/components/navbar/navbar.tsx
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import Octicon, {FileDirectory, Bell, Gear} from '@primer/octicons-react';
 
 import './navbar.scss';
 
 import Breadcrumb from './../breadcrumb/breadcrumb';
 
 
-class Navbar extends Component {
+class Navbar extends Component<{history: any}, {}> {
+  constructor(props:any) {
+    super(props)
+
+    this.onClickLogout = this.onClickLogout.bind(this)
+    this.onClickUpload = this.onClickUpload.bind(this)
+  }
+  onClickLogout(event:React.MouseEvent<HTMLButtonElement>): any {
+    this.props.history.push('/logout')
+  }
+  onClickUpload(event:React.MouseEvent<HTMLButtonElement>): any {
+    this.props.history.push('/upload')
+  }
   render() {
     return (
       <div className="nav-header">
@@ -26,6 +39,14 @@ class Navbar extends Component {
               </li>
             </ul>
           </div>
+          <form className="form-inline">
+            <button type="button" className="btn" id="btn-control" style={{marginRight: "1.5rem", border: "none"}} onClick={this.onClickUpload}>
+              <Octicon icon={FileDirectory} size="medium" />
+            </button>
+            <button type="button" className="btn" id="btn-control" style={{marginRight: "1.5rem", border: "none"}} onClick={this.onClickLogout}>
+              <Octicon icon={Gear} size="medium" />
+            </button>
+          </form>
         </nav>
       </header>
     </div>
@@ -34,4 +55,4 @@ class Navbar extends Component {
 }
 
 
-export default Navbar;
+export default withRouter(Navbar);
