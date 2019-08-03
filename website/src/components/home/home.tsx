aac014aa64c3cc12cbd44b83d157c51d27873d29
Nick <37081556+naustica@users.noreply.github.com>
2019-08-03 22:35:36 +0200

    autocomplete feature

--- website/src/components/home/home.tsx
+++ website/src/components/home/home.tsx
@@ -1,20 +1,43 @@
 import React, {Component} from 'react';
-import {withRouter} from 'react-router-dom';
+import {withRouter, Link} from 'react-router-dom';
 
 import './home.scss'
 
 
-class Home extends Component<{ history: any }, { term: string }> {
+class Home extends Component<{ history: any }, { term: string, options: Array<any>, suggestions: Array<any>}> {
   constructor(props:any) {
     super(props)
     this.state = {
-      term: ''
+      term: '',
+      options: ['lel', 'kek', 'lul', 'lil', 'l', 'li', 'luk'],
+      suggestions: [],
     }
     this.getTerm = this.getTerm.bind(this)
     this.submitForm = this.submitForm.bind(this)
   }
   getTerm(event:React.ChangeEvent<HTMLInputElement>): void {
-    this.setState({term: event.target.value})
+    const value = event.target.value
+    let suggestions = []
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, 'i')
+    suggestions = this.state.options.sort().filter(v => regex.test(v))
+    suggestions = suggestions.slice(0, 5)
+    }
+    this.setState({term: value, suggestions: suggestions})
+  }
+  suggestionsSelected(value) {
+    this.setState({term: value, suggestions: []})
+    this.props.history.push('/result/' + value)
+  }
+  renderSuggestions() {
+    if (this.state.suggestions.length === 0) {
+      return null
+    }
+    return (
+      <ul className="search-suggestions">
+        {this.state.suggestions.map((item => <li key={item} onClick={() => this.suggestionsSelected(item)}>{item}</li>))}
+      </ul>
+    )
   }
   submitForm(event:React.FormEvent<HTMLFormElement>): any {
     var form = event.target as HTMLFormElement;
@@ -27,14 +50,15 @@ class Home extends Component<{ history: any }, { term: string }> {
         console.log(error)
         form.reset();
       })
-
   }
   render() {
+    const style = this.state.suggestions.length > 0 ? {borderBottomRightRadius: "0px", borderBottomLeftRadius: "0px", borderTop: "0px"} : {}
     return (
       <div className="container" id="searchform-container">
-        <form method="POST" onSubmit={this.submitForm}>
+        <form method="POST" onSubmit={this.submitForm} style={{position: "fixed", width: "100%"}}>
           <div className="form-group input-group-lg">
-            <input className="form-control from-control-lg" id="form-control-search" type="text" name="search" onChange={this.getTerm} autoFocus placeholder="search..."/>
+            <input className="form-control from-control-lg" id="form-control-search" type="text" name="search" style={style} value={this.state.term} onChange={this.getTerm} autoFocus placeholder="search..."/>
+            {this.renderSuggestions()}
           </div>
         </form>
       </div>
