8d8023b9e47b1a8b8fb6a8bc6b239003d4de4080
Nick <37081556+naustica@users.noreply.github.com>
2019-08-06 19:33:32 +0200

    add toogle menu

--- website/src/components/navbar/navbar.tsx
+++ website/src/components/navbar/navbar.tsx
@@ -7,37 +7,61 @@ import './navbar.scss';
 import Breadcrumb from './../breadcrumb/breadcrumb';
 
 
-class Navbar extends Component<{history: any}, {}> {
+class Navbar extends Component<{history: any}, {toggle: boolean}> {
   constructor(props:any) {
     super(props)
+    this.state = {
+      toggle: false
+    }
 
-    this.onClickLogout = this.onClickLogout.bind(this)
+    this.onClickToggleMenu = this.onClickToggleMenu.bind(this)
     this.onClickUpload = this.onClickUpload.bind(this)
+    this.onWindowClick = this.onWindowClick.bind(this)
   }
-  onClickLogout(event:React.MouseEvent<HTMLButtonElement>): any {
-    this.props.history.push('/logout')
+  componentDidMount() {
+    window.addEventListener('click', this.onWindowClick);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onWindowClick);
+  }
+  onClickToggleMenu(event:React.MouseEvent<HTMLButtonElement>): any {
+    const toggle = this.state.toggle
+    this.setState({toggle: !toggle})
   }
   onClickUpload(event:React.MouseEvent<HTMLButtonElement>): any {
     this.props.history.push('/upload')
   }
+  onWindowClick(event):any {
+    if (event.target.name !== 'settings' && event.target.tagName.toLowerCase() !== 'path' && event.target.tagName.toLowerCase() !== 'a' && event.target.tagName.toLowerCase() !== 'li' && event.target !== document.querySelector('[aria-label="settings"]')) {
+      this.setState({toggle: false})
+    }
+    console.log(event.target)
+  }
   render() {
+    const toggleStyle = this.state.toggle ? {display: "inline"} : {display: "none"}
     return (
       <div className="nav-header">
       <header>
-        <nav className="navbar navbar-expand-lg navbar-light">
+        <nav className="navbar navbar-expand-lg navbar-light" style={{padding: 0}}>
           <Link to='/' className="navbar-brand" style={{paddingLeft: "1.5rem"}}>nickipedia</Link>
           <div className="collapse navbar-collapse">
           </div>
-          <form className="form-inline">
-            <button type="button" className="btn" id="btn-control" style={{marginRight: "1.5rem", border: "none"}} onClick={this.onClickUpload}>
-              <Octicon icon={FileDirectory} size="medium" />
-            </button>
-            <button type="button" className="btn" id="btn-control" style={{marginRight: "1.5rem", border: "none"}} onClick={this.onClickLogout}>
-              <Octicon icon={Gear} size="medium" />
-            </button>
-          </form>
+          <button type="button" className="btn" id="btn-control" style={{marginRight: "1.5rem", border: "none"}} onClick={this.onClickUpload}>
+            <Octicon icon={FileDirectory} size="medium" />
+          </button>
+          <div>
+          <button type="button" className="btn" id="btn-control" name="settings" style={{marginRight: "1.5rem", border: "none", height: "100%"}} onClick={this.onClickToggleMenu}>
+            <Octicon icon={Gear} size="medium" ariaLabel="settings"/>
+          </button>
+          </div>
         </nav>
       </header>
+      <div className="toggle-menu" style={toggleStyle}>
+        <ul>
+          <Link to={'/settings'} style={{color: "black"}}><li>settings</li></Link>
+          <Link to={'/logout'} style={{color: "black"}}><li>logout</li></Link>
+        </ul>
+      </div>
     </div>
   )
   }
