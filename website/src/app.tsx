373756510bb6b9674014765782c501b57418e953
Nick <37081556+naustica@users.noreply.github.com>
2019-08-06 15:50:31 +0200

    change page structure

--- website/src/app.tsx
+++ website/src/app.tsx
@@ -33,28 +33,20 @@ class App extends Component {
       return (
           <Router>
             <Route path={['/', '/home', '/settings', '/about', '/result/:term', '/watch/:id', '/upload']} exact component={Navbar} />
-              <div className="app">
-                  <div className="container">
-                      <div className="row">
-                          <div className="col">
-                              <div>
-                                <Switch>
-                                  <AuthenticatedRoute path='/' exact component={Home}/>
-                                  <Route path='/home' exact component={Home}/>
-                                  <Route path='/settings' component={Settings}/>
-                                  <Route path='/about' component={About}/>
-                                  <Route path='/login' exact component={Login}/>
-                                  <Route path='/register' exact component={Register}/>
-                                  <AuthenticatedRoute path='/upload' exact component={Upload}/>
-                                  <AuthenticatedRoute path='/logout' exact component={Logout}/>
-                                  <Route path='/result/:term' component={Results}/>
-                                  <Route path='/watch/:id' component={Video}/>
-                                  <Route component={Error}/>
-                                </Switch>
-                              </div>
-                          </div>
-                        </div>
-                    </div>
+              <div className="app" style={{height: "100%"}}>
+                  <Switch>
+                    <AuthenticatedRoute path='/' exact component={Home}/>
+                    <Route path='/home' exact component={Home}/>
+                    <Route path='/settings' component={Settings}/>
+                    <Route path='/about' component={About}/>
+                    <Route path='/login' exact component={Login}/>
+                    <Route path='/register' exact component={Register}/>
+                    <AuthenticatedRoute path='/upload' exact component={Upload}/>
+                    <AuthenticatedRoute path='/logout' exact component={Logout}/>
+                    <Route path='/result/:term' component={Results}/>
+                    <Route path='/watch/:id' component={Video}/>
+                    <Route component={Error}/>
+                  </Switch>
                 </div>
           </Router>
       )
