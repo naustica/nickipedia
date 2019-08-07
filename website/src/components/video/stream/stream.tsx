c8691df3a5916e2a087060a439385411a9ab91a0
Nick <37081556+naustica@users.noreply.github.com>
2019-08-07 14:06:08 +0200

    add text overlay by disconnect

--- website/src/components/video/stream/stream.tsx
+++ website/src/components/video/stream/stream.tsx
@@ -3,15 +3,42 @@ import React, {Component} from 'react';
 import './../video.scss'
 
 
-class VideoStream extends Component<{author: string, filename: string}, {}> {
+class VideoStream extends Component<{author: string, filename: string}, {error: boolean}> {
   constructor(props:any) {
     super(props)
+    this.state = {
+      error: false,
+    }
+  }
+  async componentDidMount() {
+    try {
+      const video = await fetch('http://0.0.0.0:8000/videos/' + this.props.author + '/' + this.props.filename, {
+        method: 'head'
+      })
+      /*
+      const image = await fetch('http://0.0.0.0:8000/default/background.jpg', {
+        method: 'head'
+      })
+      */
+      this.setState({error: false})
+
+    }
+    catch {
+      this.setState({error: true})
+      console.log('[nickipedia] file server didnt respond')
+    }
   }
   render() {
+    const errorOverlay = this.state.error ? {opacity: 1} : {opacity: 0}
     return (
-      <video className="card-img-top" poster={"http://0.0.0.0:8000/default/background.jpg"} id="player" style={{width:"100%", borderRadius: "5px", outline: "none"}} playsInline controls>
-        <source src={"http://0.0.0.0:8000/videos/" + this.props.author + "/" + this.props.filename} type="video/mp4"/>
-      </video>
+      <div>
+        <video className="card-img-top" poster={""} id="player" style={{width:"100%", height: "auto", borderRadius: "5px", outline: "none", backgroundColor: "#0A0A0A"}} playsInline controls>
+          <source src={'http://0.0.0.0:8000/videos/' + this.props.author + '/' + this.props.filename} type="video/mp4"/>
+        </video>
+        <div id="video-overlay" style={errorOverlay}>
+          <h3>video not found.</h3>
+        </div>
+      </div>
     )
   }
 }
