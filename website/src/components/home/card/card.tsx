0bbf25b7c62481affef35e15f1dc46f8747baf6c
Nick <37081556+naustica@users.noreply.github.com>
2019-08-10 22:54:02 +0200

    add timestamps

--- website/src/components/home/card/card.tsx
+++ website/src/components/home/card/card.tsx
@@ -2,18 +2,23 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import Octicon, {Heart, CommentDiscussion} from '@primer/octicons-react';
 
-import './../home.scss'
+import './../home.scss';
+import Loading from './../../loading/loading';
+import ConvertTime from './../../../utils/datetime';
 
 
-class Card extends Component<{result: any}, {likes: number, comments: number}> {
+class Card extends Component<{result: any}, {likes: number, comments: number, timestamp: string, loading: boolean}> {
   constructor(props:any) {
     super(props)
     this.state = {
       likes: 0,
-      comments: 0
+      comments: 0,
+      timestamp: null,
+      loading: false
     }
     this.computeLikes = this.computeLikes.bind(this)
     this.computeComments = this.computeComments.bind(this)
+    this.computeDate = this.computeDate.bind(this)
   }
   computeLikes() {
     var i
@@ -33,19 +38,26 @@ class Card extends Component<{result: any}, {likes: number, comments: number}> {
       }
       this.setState({comments: comments})
   }
-
+  computeDate() {
+    let data:any = [{timestamp: this.props.result.timestamp}]
+    ConvertTime(data)
+    this.setState({timestamp: data[0].timestamp})
+  }
   componentDidMount() {
+    this.setState({loading: true})
     this.computeLikes()
     this.computeComments()
+    this.computeDate()
+    this.setState({loading: false})
   }
   render() {
-    return (
+    const loadingState = this.state.loading ? (<div className="card" id="front-page-suggestions"><Loading loading={this.state.loading}/></div>) : (
       <div className="card" id="front-page-suggestions">
         <Link to={'/watch/' + this.props.result.id} style={{color: "black"}}>
           <img className="card-img-top" src="http://0.0.0.0:8000/default/default_thumbnail.jpg" style={{objectFit: "cover"}} alt="..." />
           <div className="card-body" style={{borderTop: "2px solid #505458", padding: "0.1rem"}}>
             <p className="card-text" style={{textAlign: "left", fontSize: "13px", fontWeight: 500, position: "relative", margin: 0}}>{this.props.result.title}</p>
-            <div style={{margin: 0, paddingLeft: "0.5rem", paddingTop: 0, paddingRight: "0.5rem", paddingBottom: 0, position: "absolute", bottom: 0}}>
+            <div style={{margin: 0, paddingLeft: "0.5rem", paddingTop: 0, paddingRight: "0.5rem", paddingBottom: 0, position: "absolute", bottom: 0, left: 0}}>
               <div style={{color: "#E0235F", display:"inline", paddingLeft: "0.5rem", paddingTop: 0, paddingRight: "0.5rem", paddingBottom: 0}}>
                 <Octicon icon={Heart} size="small" />
               </div>
@@ -59,10 +71,16 @@ class Card extends Component<{result: any}, {likes: number, comments: number}> {
                 {this.state.comments}
               </div>
             </div>
+            <div style={{margin: 0, paddingLeft: "0.5rem", paddingTop: 0, paddingRight: "0.5rem", paddingBottom: 0, position: "absolute", bottom: 0, right: 0}}>
+              <div style={{display:"inline", color: "#757D85", fontSize: "13px"}}>
+                {this.state.timestamp}
+              </div>
+            </div>
           </div>
         </Link>
       </div>
-  )
+    )
+    return loadingState
   }
 }
 
