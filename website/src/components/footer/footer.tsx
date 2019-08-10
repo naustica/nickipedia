d9431be9167adda89995937aab6a81bcad7a296c
Nick <37081556+naustica@users.noreply.github.com>
2019-08-10 13:27:31 +0200

    add footer component

---
+++ website/src/components/footer/footer.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+import './footer.scss';
+
+
+class Footer extends Component {
+
+    render() {
+      return (
+          <div className="footer">nickipedia 2019</div>
+      )
+    }
+
+}
+
+
+export default Footer
