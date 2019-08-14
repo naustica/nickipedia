74b19ff61568fafc49d7991850bcf0070f73f0f7
Nick <37081556+naustica@users.noreply.github.com>
2019-08-14 13:12:12 +0200

    sorting results

--- website/src/components/results/card/card.tsx
+++ website/src/components/results/card/card.tsx
@@ -2,18 +2,56 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import Octicon, {TriangleRight} from '@primer/octicons-react';
 
-import './../results.scss'
+import './../results.scss';
+import ConvertTime from './../../../utils/datetime';
 
 
-class Card extends Component<{result:any}, {maxLength: number}> {
+class Card extends Component<{result:any}, {maxLength: number, likes: number, comments: number, timestamp: any, loading: boolean}> {
   constructor(props:any) {
     super(props)
     this.state = {
-      maxLength: 200
+      maxLength: 200,
+      likes: 0,
+      comments: 0,
+      timestamp: null,
+      loading: false
+    }
+    this.computeLikes = this.computeLikes.bind(this)
+    this.computeComments = this.computeComments.bind(this)
+    this.computeDate = this.computeDate.bind(this)
+  }
+  computeLikes() {
+    var i
+    let likes = 0
+    for (i=0; i < this.props.result.voting.length; i++) {
+      if (this.props.result.voting[i].like === 1) {
+        likes++
+      }
+      this.setState({likes: likes})
     }
   }
+  computeComments() {
+    var i
+    let comments = 0
+    for (i=0; i < this.props.result.comments.length; i++) {
+        comments++
+      }
+      this.setState({comments: comments})
+  }
+  computeDate() {
+    let data:any = [{timestamp: this.props.result.timestamp}]
+    ConvertTime(data)
+    this.setState({timestamp: data[0].timestamp})
+  }
+  componentDidMount() {
+    this.setState({loading: true})
+    this.computeLikes()
+    this.computeComments()
+    this.computeDate()
+    this.setState({loading: false})
+  }
   render() {
-    const checkLengthDescription = this.props.result.properties.text.length > this.state.maxLength ? this.props.result.properties.text.substring(0, this.state.maxLength) + '...' : this.props.result.properties.text
+    const checkLengthDescription = this.props.result.text.length > this.state.maxLength ? this.props.result.text.substring(0, this.state.maxLength) + '...' : this.props.result.text
     return (
         <div className="card mb-2" id="result-card">
           <Link to={'/watch/' + this.props.result.id} style={{color: "black"}}>
@@ -26,7 +64,8 @@ class Card extends Component<{result:any}, {maxLength: number}> {
             </div>
             <div className="col-md-9">
               <div className="card-body">
-                <h5 className="card-title" style={{}}>{this.props.result.properties.title}</h5>
+                <h6 className="card-title" style={{marginBottom: "0.5rem", fontSize: "16px"}}>{this.props.result.title}</h6>
+                <p style={{fontSize: "12px", color: "#6D6D6D", marginBottom: "0.5rem"}}>{this.props.result.author_id} • {this.props.result.views + ' views'} • {this.state.timestamp}</p>
                 <p className="card-text" style={{fontSize: "12px"}}>{checkLengthDescription}</p>
               </div>
             </div>
--- website/src/components/results/results.tsx
+++ website/src/components/results/results.tsx
@@ -7,42 +7,67 @@ import Card from './card/card';
 import Loading from './../loading/loading';
 
 
-class Results extends Component<{match: any}, {resultComponent: any, errors: boolean, term: string, loading: boolean}> {
+class Results extends Component<{match: any}, {errors: boolean, term: string, table: string, data: any, loading: boolean}> {
   constructor(props:any) {
     super(props)
     this.state = {
-      resultComponent: null,
       errors: false,
       term: '',
+      data: [],
+      table: 'video',
       loading: true
     }
     this.getResults = this.getResults.bind(this)
+    this.updateSorting = this.updateSorting.bind(this)
   }
-  getResults(term) {
+  getResults(term, table) {
     this.setState({errors: false})
-    fetch('api/search?term=' + term, {
+    fetch('api/search?term=' + term + '&table=' + table, {
       method: 'get',
     })
     .then ((response => response.json()))
     .then((data) => {
-    this.setState({resultComponent: data.map(result => <Card key={result.id} result={result} />), term: term, loading: false})
+      this.sortResults(data, 'id')
+      this.setState({term: term, data: data, loading: false})
     })
     .catch(error => {
       console.log(error)
       this.setState({errors: true})
     })
   }
+  updateSorting(value:any):any {
+    const data = this.state.data
+    this.sortResults(data, value)
+    this.setState({data: data})
+  }
+  sortResults(data, value) {
+    switch(true) {
+      case value === 'id':
+        data = data.sort((a, b) => a.id - b.id)
+        break
+      case value === 'date':
+        data = data.sort((a, b) => a.timestamp - b.timestamp)
+        break
+      case value === 'views':
+        data = data.sort((a, b) => b.views - a.views)
+        break
+      case value === 'rating':
+        data = data.sort((a, b) => a.id - b.id)
+        break
+    }
+  }
   componentDidMount() {
     const term = this.props.match.params.term
-    this.getResults(term)
+    this.getResults(term, this.state.table)
   }
   componentDidUpdate(prevProps) {
     if (this.props.match.params.term != prevProps.match.params.term) {
       const term = this.props.match.params.term
-      this.getResults(term)
+      this.getResults(term, this.state.table)
     }
   }
   render() {
+    const resultComponent = this.state.data.map(result => <Card key={result.id} result={result} />)
     const loadingState = this.state.loading ? (<Loading loading={this.state.loading}/>) : (
       <div className="container" style={{paddingTop: "2rem"}}>
         <div className="row">
@@ -54,17 +79,17 @@ class Results extends Component<{match: any}, {resultComponent: any, errors: boo
                 <h6 className="card-subtitle" style={{padding: "2rem"}}><b>FILTER</b></h6>
                 <Octicon icon={Settings} size="medium" />
                   <ul className="list-group" style={{listStyleType: "none", textAlign: "center", margin: 0, padding: 0, paddingTop: "1rem"}}>
-                    <li className="list-group-item">date</li>
-                    <li className="list-group-item">view count</li>
-                    <li className="list-group-item">rating</li>
-                    <li className="list-group-item">video</li>
-                    <li className="list-group-item">pooper</li>
+                    <li className="list-group-item" value="date" onClick={() => this.updateSorting('date')}>date</li>
+                    <li className="list-group-item" value="views" onClick={() => this.updateSorting('views')}>view count</li>
+                    <li className="list-group-item" value="rating" onClick={() => this.updateSorting('rating')}>rating</li>
+                    <li className="list-group-item" value="video" onClick={() => this.updateSorting('video')}>video</li>
+                    <li className="list-group-item" value="pooper" onClick={() => this.updateSorting('pooper')}>pooper</li>
                   </ul>
                 </div>
               </div>
             </div>
             <div className="col-9" style={{padding: 0, paddingBottom: "2rem"}}>
-              {this.state.resultComponent}
+              {resultComponent}
             </div>
           </div>
         </div>
