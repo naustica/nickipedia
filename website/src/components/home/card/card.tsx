4602654cb38030156b7b9d2de51d7ace068fffa3
Nick <37081556+naustica@users.noreply.github.com>
2019-08-16 01:18:42 +0200

    design changes + redux store functionality

--- website/src/components/home/card/card.tsx
+++ website/src/components/home/card/card.tsx
@@ -7,11 +7,11 @@ import Loading from './../../loading/loading';
 import ConvertTime from './../../../utils/datetime';
 
 
-class Card extends Component<{result: any}, {maxLength: number, likes: number, comments: number, timestamp: string, loading: boolean}> {
+class Card extends Component<{result: any, loading}, {maxLength: number, likes: number, comments: number, timestamp: string, loading: boolean}> {
   constructor(props:any) {
     super(props)
     this.state = {
-      maxLength: 55,
+      maxLength: 100,
       likes: 0,
       comments: 0,
       timestamp: null,
@@ -53,13 +53,15 @@ class Card extends Component<{result: any}, {maxLength: number, likes: number, c
   }
   render() {
     const checkLengthTitle = this.props.result.title.length > this.state.maxLength ? this.props.result.title.substring(0, this.state.maxLength) + '...' : this.props.result.title
-    const loadingState = this.state.loading ? (<div className="card" id="front-page-suggestions"><Loading loading={this.state.loading}/></div>) : (
+    const loadingState = this.props.loading ? (<div className="col-3"><div className="card" id="front-page-suggestions" style={{backgroundColor: "#E0DFDF"}}></div></div>) : (
+      <div className="col-3">
       <div className="card" id="front-page-suggestions">
         <Link to={'/watch/' + this.props.result.id} style={{color: "black"}}>
         <div style={{height: "100%", position: "absolute"}}>
-          <img className="card-img-top" src="http://0.0.0.0:8000/default/default_thumbnail.jpg" style={{objectFit: "cover"}} alt="..." />
-          <div className="card-body" style={{borderTop: "2px solid #505458", padding: "0.1rem"}}>
-            <p className="card-text" style={{textAlign: "left", fontSize: "13px", fontWeight: 500, position: "relative", margin: 0}}>{checkLengthTitle}</p>
+          <img className="card-img-top" src="http://0.0.0.0:8000/default/default_thumbnail.jpg" style={{objectFit: "cover", height: "50%"}} alt="..." />
+          <div className="card-body" style={{borderTop: "1px solid #505458", padding: "0.1rem"}}>
+            <p className="card-text" style={{textAlign: "left", fontSize: "14px", fontWeight: 500, position: "relative", margin: 0, height: "auto"}}>{checkLengthTitle}</p>
+            <p className="card-text" style={{textAlign: "left", fontSize: "14px", fontWeight: 500, color: "#6D6D6D", position: "relative", height: "auto", paddingTop: "0.5rem"}}>{this.props.result.views + " views"}</p>
             <div style={{margin: 0, paddingLeft: "0.5rem", paddingTop: 0, paddingRight: "0.5rem", paddingBottom: 0, position: "absolute", bottom: 0, left: 0}}>
               <div style={{color: "#E0235F", display:"inline", paddingLeft: "0.5rem", paddingTop: 0, paddingRight: "0.5rem", paddingBottom: 0}}>
                 <Octicon icon={Heart} size="small" />
@@ -82,6 +84,7 @@ class Card extends Component<{result: any}, {maxLength: number, likes: number, c
           </div>
           </div>
         </Link>
+      </div>
       </div>
     )
     return loadingState
--- website/src/components/home/home.tsx
+++ website/src/components/home/home.tsx
@@ -5,111 +5,91 @@ import {connect} from 'react-redux';
 import './home.scss'
 
 import Card from './card/card';
-import Loading from './../loading/loading';
 
-import {fetchVideo, fetchVideoStart, addView} from './../../store/actions/videoActions';
+import {fetchVideos, fetchVideoStart, getVideoSuggestions, getVideoSuggestionsStart} from './../../store/actions/videoActions';
 
 
 @(connect((store: any) => {
   return {
-    video: store.video
+    videos: store.video
   }
 }) as any)
-class Home extends Component<{dispatch?: any, video?: any},{loading: boolean, resultComponent: any, errors: boolean}> {
+class Home extends Component<{dispatch?: any, videos?: any},{}> {
   constructor(props:any) {
     super(props)
-    this.state = {
-      loading: false,
-      resultComponent: null,
-      errors: false
-    }
   }
   componentWillMount() {
-    // redux test 
     Promise.all([
       this.props.dispatch(fetchVideoStart()),
-      this.props.dispatch(fetchVideo(1))
+      this.props.dispatch(getVideoSuggestionsStart()),
+      this.props.dispatch(fetchVideos())
     ]).then(() => {
-      this.props.dispatch(addView(1, this.props.video.views))})
+      this.props.dispatch(getVideoSuggestions(null, 16))
+    })
   }
-  componentDidMount() {
-    this.setState({loading: true})
-    fetch('api/video?all=True', {
-      method: 'get',
-    })
-    .then ((response => response.json()))
-    .then((data) => {
-      let suggestions = []
-      for (var i=0; i < 6; i++) {
-        let index = Math.floor(Math.random() * Object.keys(data).length)
-        suggestions.push(data[index])
-        data.splice(index, 1)
-      }
-      this.setState({resultComponent: suggestions.map(result => <Card key={result.id} result={result} />), loading: false})
-    })
-    .catch(error => {
-      console.log(error)
-      this.setState({errors: true})
-    })
+  renderCards(begin: number, end: number) {
+    return (this.props.videos.suggestions.slice(begin, end).map(result => <Card key={result.id} result={result} loading={this.props.videos.changing}/>))
   }
   render() {
-    const loadingState = this.state.loading ? (<Loading loading={this.state.loading}/>) : (
-      <div className="container" style={{paddingTop: "2rem"}}>
+    const style = this.props.videos.changing ? {backgroundColor: "#E0DFDF", color: "transparent", boxShadow: "none", border: "none"} : {}
+    return (
+      <div className="container" id="home-container">
         <div className="row">
-          <div className="col-3" style={{padding: 0}}>
+          <div className="col-2">
             <div className="card" id="trends-card">
-              <div className="card-body">
-                <h2 className="card-title">trends</h2>
+              <div className="card-body" style={style}>
+                <h3 className="card-title">trends</h3>
                 <ul>
-                  <Link to={"/"}><li>#kek</li></Link>
-                  <Link to={"/"}><li>#lel</li></Link>
-                  <Link to={"/"}><li>#lul</li></Link>
-                  <Link to={"/"}><li>#kieling</li></Link>
-                  <Link to={"/"}><li>#marcel davis</li></Link>
-                  <Link to={"/"}><li>#bachenbenno</li></Link>
-                  <Link to={"/"}><li>#fistus</li></Link>
-                  <Link to={"/"}><li>#saas</li></Link>
-                  <Link to={"/"}><li>#pooper</li></Link>
-                  <Link to={"/"}><li>#laal</li></Link>
+                  <Link to={"/"} style={style}><li>#kek</li></Link>
+                  <Link to={"/"} style={style}><li>#lel</li></Link>
+                  <Link to={"/"} style={style}><li>#lul</li></Link>
+                  <Link to={"/"} style={style}><li>#kieling</li></Link>
+                  <Link to={"/"} style={style}><li>#marcel davis</li></Link>
+                  <Link to={"/"} style={style}><li>#bachenbenno</li></Link>
+                  <Link to={"/"} style={style}><li>#fistus</li></Link>
+                  <Link to={"/"} style={style}><li>#saas</li></Link>
+                  <Link to={"/"} style={style}><li>#pooper</li></Link>
+                  <Link to={"/"} style={style}><li>#laal</li></Link>
                 </ul>
               </div>
             </div>
             <div className="card" id="links-card">
-              <div className="card-body">
-                <h3 className="card-title">key links</h3>
+              <div className="card-body" style={style}>
+                <h4 className="card-title">key links</h4>
                 <ul>
-                  <Link to={"/"}><li>about</li></Link>
-                  <Link to={"/"}><li>contact</li></Link>
-                  <Link to={"/"}><li>faq</li></Link>
-                  <Link to={"/"}><li>github</li></Link>
-                  <Link to={"/"}><li>twitter</li></Link>
-                  <Link to={"/"}><li>privacy policy</li></Link>
-                  <Link to={"/"}><li>terms of use</li></Link>
-                  <Link to={"/"}><li>code of conduct</li></Link>
+                  <Link to={"/"} style={style}><li>about</li></Link>
+                  <Link to={"/"} style={style}><li>contact</li></Link>
+                  <Link to={"/"} style={style}><li>faq</li></Link>
+                  <Link to={"/"} style={style}><li>github</li></Link>
+                  <Link to={"/"} style={style}><li>twitter</li></Link>
+                  <Link to={"/"} style={style}><li>privacy policy</li></Link>
+                  <Link to={"/"} style={style}><li>terms of use</li></Link>
+                  <Link to={"/"} style={style}><li>code of conduct</li></Link>
                 </ul>
               </div>
             </div>
           </div>
-          <div className="col-9" style={{padding: 0, paddingBottom: "2rem", marginTop: "1rem"}}>
+          <div className="col-10">
           <div className="frontpage-tabs">
             <ul>
-              <Link to={"/"}><li>feed</li></Link>
-              <Link to={"/"}><li>recommended</li></Link>
-              <Link to={"/"}><li>classics</li></Link>
+              <Link to={"/"}><li style={style}>feed</li></Link>
+              <Link to={"/"}><li style={style}>recommended</li></Link>
+              <Link to={"/"}><li style={style}>classics</li></Link>
             </ul>
           </div>
-            <div className="row" style={{marginLeft: "1rem"}}>
-              {this.state.resultComponent}
-              <div style={{width: "100%", padding: "1rem", textAlign: "center"}}>
-                <Link to="/"><h2 className="frontpage-suggestions-h">suggestions</h2></Link>
-              </div>
-              {this.state.resultComponent}
+            <div className="row">
+                {this.renderCards(0, 8)}
+            </div>
+            <div style={{width: "100%", padding: "1rem", textAlign: "center"}}>
+              <Link to="/"><h2 className="frontpage-suggestions-h" style={style}>suggestions</h2></Link>
+            </div>
+            <div className="row">
+              {this.renderCards(8, 16)}
+            </div>
             </div>
           </div>
         </div>
-      </div>
     )
-    return loadingState
   }
 }
 
