5525573add84677a9558f64354363b4937cac743
Nick <37081556+naustica@users.noreply.github.com>
2019-08-16 01:20:24 +0200

    smoother rendering

--- website/src/components/video/suggestions/suggestions.tsx
+++ website/src/components/video/suggestions/suggestions.tsx
@@ -1,48 +1,34 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-import './../video.scss'
+import './../video.scss';
+import Loading from './../../loading/loading';
 
 
-class VideoSuggestions extends Component<{id?: number}, {data: Array<any>}> {
+class VideoSuggestions extends Component<{suggestions: any, loading: boolean},{}> {
   constructor(props:any) {
     super(props)
-    this.state = {
-      data: []
-    }
-    this.getSuggestions = this.getSuggestions.bind(this)
   }
-  async getSuggestions(id) {
-    await fetch('api/video?all=True', {
-      method: 'get'
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      this.setState({data: data})
-      const newData = this.state.data.filter(item => item.id !== Number(id))
-      this.setState({data: newData})
-    })
-  }
-  componentDidMount() {
-    this.getSuggestions(this.props.id)
-  }
-  componentDidUpdate(prevProps) {
-    if (this.props.id !== prevProps.id) {
-      this.getSuggestions(this.props.id)
-    }
+  renderSuggestionsCards(loading) {
+    const content = loading ? this.props.suggestions.map(suggestion => (
+      <div className="card" key={suggestion.id} style={{height: "10rem", width: "14rem", backgroundColor: "#E0DFDF", margin: "1rem", marginBottom: "2rem"}}>
+      </div>
+    )) : (
+      this.props.suggestions.slice(0, 6).map(suggestion => (
+        <div className="card" key={suggestion.id} style={{height: "10rem", width: "14rem", backgroundColor: "transparent", margin: "1rem", marginBottom: "2rem"}}>
+          <Link to={"/watch/" + suggestion.id}>
+            <img src="http://0.0.0.0:8000/default/default_thumbnail.jpg" className="card-img-top" alt="..." style={{borderRadius: "5px"}} />
+            <div className="card-img-overlay">
+              <h5 className="card-title" style={{fontSize: "12px", color: "black"}}>{suggestion.title}</h5>
+            </div>
+          </Link>
+        </div>
+      ))
+    )
+    return content
   }
   render() {
-    const suggestionsCards= (this.state.data.slice(0, 3).map(suggestion => (
-      <div className="card" key={suggestion.id} style={{height: "10rem", width: "14rem", backgroundColor: "transparent", margin: "1rem", marginBottom: "2rem", borderRadius: "5px"}}>
-        <Link to={"/watch/" + suggestion.id}>
-          <img src="http://0.0.0.0:8000/default/default_thumbnail.jpg" className="card-img-top" alt="..." style={{borderRadius: "5px"}} />
-          <div className="card-img-overlay">
-            <h5 className="card-title" style={{fontSize: "12px", color: "black"}}>{suggestion.title}</h5>
-          </div>
-        </Link>
-      </div>
-    )))
-    return suggestionsCards
+    return this.renderSuggestionsCards(this.props.loading)
   }
 }
 
