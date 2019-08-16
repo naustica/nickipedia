2ec8fcfb4fed5949ff4e1984bfa611050074b761
Nick <37081556+naustica@users.noreply.github.com>
2019-08-16 17:32:13 +0200

    add searchReducer

--- website/src/store/store.tsx
+++ website/src/store/store.tsx
@@ -3,11 +3,13 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 
-import videoReducer from './reducers/videoReducer'
+import videoReducer from './reducers/videoReducer';
+import searchReducer from './reducers/searchReducer';
 
 
 const reducers = combineReducers({
-  video: videoReducer
+  video: videoReducer,
+  search: searchReducer
 })
 const middleware = applyMiddleware(promise, thunk, logger)
 
