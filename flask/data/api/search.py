35e1e3cfe14157abad68c5f39368203a2efb91c9
Nick <37081556+naustica@users.noreply.github.com>
2019-08-16 17:29:02 +0200

    add limit and offset to load data in chunks

--- flask/data/api/search.py
+++ flask/data/api/search.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, make_response, request
 from data.database.video import Video, videos_schema
+from data import db
+from sqlalchemy import func
 
 
 bp = Blueprint('search_api', __name__, url_prefix='/api')
@@ -8,21 +10,40 @@ bp = Blueprint('search_api', __name__, url_prefix='/api')
 @bp.route('/search', methods=['GET'])
 def get_results():
     """
-    example: GET: host/api/search?term=nickipedia&table=video
+    example: GET: host/api/search?term=nickipedia&table=video?page=1
     """
 
     term = request.args.get('term', default='', type=str)
 
     table = request.args.get('table', default='video', type=str)
 
+    page = request.args.get('page', default='1', type=int)
+
+    start = 0
+
+    stop = 10
+
     if table == 'video':
 
-        results = Video.query.filter(Video.title.ilike('{}{}{}'.format('%', term, '%'))).all()
+        all_rows = db.session.query(func.count(Video.id)).scalar()
 
-        if not results:
-            return make_response(jsonify(message='no results')), 200
+        page = int(page)
 
-        return make_response(videos_schema.jsonify(results)), 200
+        page_limit = all_rows / stop
+
+        if page <= page_limit:
+
+            start = (page - 1) * stop
+
+            results = Video.query.filter(Video.title.ilike('{}{}{}'.format('%', term, '%'))).offset(start).limit(stop).all()
+
+            if not results:
+                return make_response(jsonify([])), 200
+
+            return make_response(videos_schema.jsonify(results)), 200
+
+        else:
+            return make_response(jsonify([])), 200
 
     else:
-        return make_response(jsonify(message='no results')), 200
+        return make_response(jsonify([])), 200
