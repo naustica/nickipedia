77f28112e1d3431cc4ff3d53ea8d5abb265f3bcc
Nick <37081556+naustica@users.noreply.github.com>
2019-08-22 16:55:06 +0200

    add docker deployment

---
+++ website/webpack.config.docker.js
@@ -0,0 +1,75 @@
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var {CleanWebpackPlugin} = require('clean-webpack-plugin');
+var CompressionPlugin = require('compression-webpack-plugin');
+var {GenerateSW} = require('workbox-webpack-plugin');
+
+
+module.exports = {
+  mode: "production",
+  devtool: "none",
+  entry: ["./src/app.tsx"],
+  performance: {
+    maxEntrypointSize: 700000,
+    maxAssetSize: 700000
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js"
+  },
+  watch: false,
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", "json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: "awesome-typescript-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: "url-loader"
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      dry: true,
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+      inject: false,
+      minify: {
+        removeAttributeQuotes: false,
+        collapseWhitespace: false,
+        removeComments: false
+      }
+    }),
+    new CompressionPlugin({
+      filename: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: false
+    }),
+    new GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: /.*/,
+          handler: 'CacheFirst'
+        }
+      ]
+    })
+  ]
+};
