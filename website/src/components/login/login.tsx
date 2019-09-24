56dfdfe49fcdcbc955aa9f4b122ecca2d4d2c910
Nick <37081556+naustica@users.noreply.github.com>
2019-09-24 20:00:29 +0200

    better error responses and sick animations

--- website/src/components/login/login.tsx
+++ website/src/components/login/login.tsx
@@ -5,8 +5,22 @@ import './login.scss'
 
 import Loading from './../loading/loading';
 
+interface ReadOnly {
+  history:any
+}
 
-class Login extends Component<{history:any}, { username?: string, password?: string, access_token?: string, loading?: boolean, error?: string }> {
+interface WriteOnly {
+  username?: string,
+  password?: string,
+  access_token?: string,
+  loading?: boolean,
+  error?: string,
+  errorUsername?: string,
+  errorPassword?: string
+}
+
+
+class Login extends Component<ReadOnly, WriteOnly> {
   constructor(props:any) {
     super(props)
     this.state = {
@@ -14,18 +28,28 @@ class Login extends Component<{history:any}, { username?: string, password?: str
       password: '',
       access_token: '',
       loading: false,
-      error: ''
+      error: '',
+      errorUsername: '',
+      errorPassword: ''
+
     }
     this.onChange = this.onChange.bind(this)
     this.submitForm = this.submitForm.bind(this)
-    this.validateForm = this.validateForm.bind(this)
   }
   onChange(event:React.ChangeEvent<HTMLInputElement>): void {
     this.setState({[event.target.name]: event.target.value})
   }
-  validateForm(): boolean {
-    if (this.state.username === '' || this.state.password === '') {
-      this.setState({error: '*username and password required'})
+  validateForm = (): boolean => {
+    let errorMessage: boolean = false
+    if (this.state.username === '') {
+      this.setState({errorUsername: '*Username required'})
+      errorMessage = true
+    }
+    if (this.state.password === '') {
+      this.setState({errorPassword: '*Password required'})
+      errorMessage = true
+    }
+    if (errorMessage === true) {
       return false
     }
     return true
@@ -33,6 +57,7 @@ class Login extends Component<{history:any}, { username?: string, password?: str
   submitForm(event: any): any {
     //var form = event.target as HTMLFormElement;
     event.preventDefault();
+    this.setState({error: '', errorUsername: '', errorPassword: ''})
     if (this.validateForm()) {
       this.setState({loading: true})
       fetch('api/auth/login', {
@@ -43,7 +68,7 @@ class Login extends Component<{history:any}, { username?: string, password?: str
         .then ((response => response.json()))
         .then((data) => {
           if (data.access_token === undefined) {
-            this.setState({loading: false, error: '*username or password not correct'})
+            this.setState({loading: false, error: '*Username or Password not correct'})
             //form.reset();
           } else {
             this.setState({access_token: data.access_token})
@@ -59,7 +84,6 @@ class Login extends Component<{history:any}, { username?: string, password?: str
       }
   }
   render() {
-    const formBorderColor = this.state.error != '' ? {borderColor: 'red'} : {}
     return (
       <div className="container-login">
         <div className="login-card">
@@ -70,15 +94,27 @@ class Login extends Component<{history:any}, { username?: string, password?: str
             </div>
             <form onSubmit={this.submitForm}>
               <div className="login-form-group">
-                <input className="login-form-input" style={formBorderColor} type="text" name="username" autoFocus value={this.state.username} onChange={this.onChange} placeholder="Username"/>
+                <div className="login-form-error">
+                  {this.state.errorUsername}
+                </div>
+                <input className="login-form-input" type="text" name="username" autoFocus value={this.state.username} onChange={this.onChange} placeholder="Username"/>
+                <label className="login-label">
+                  <span className="login-content">Username</span>
+                </label>
               </div>
               <div className="login-form-group">
-                <input className="login-form-input" style={formBorderColor} type="password" name="password" value={this.state.password} onChange={this.onChange} placeholder="Password"/>
+                <div className="login-form-error">
+                  {this.state.errorPassword}
+                </div>
+                <input className="login-form-input" type="password" name="password" value={this.state.password} onChange={this.onChange} placeholder="Password"/>
+                <label className="login-label">
+                  <span className="login-content">Password</span>
+                </label>
               </div>
-              <button type="button" className="login-button" onClick={this.submitForm}>Login</button>
+              <button type="button" className="login-button" onClick={this.submitForm}>Sign in</button>
               <button type="submit" style={{display: "none"}}></button>
-              <div className="login-form-group" style={{fontSize: "16px"}}>
-                Dont have an account? <Link to="/register" style={{fontSize: "16px", color: "#1873E8"}}>Sign up</Link>
+              <div className="login-form-group" style={{fontSize: "15px"}}>
+                Dont have an account? <Link to="/register" style={{fontSize: "15px", color: "#1873E8"}}>Sign up</Link>
               </div>
               <div className="login-card-loading">
                 <Loading loading={this.state.loading}/>
