7ccc605d73b43c9eb2c587f8b900c1f0a86ada7f
Nick <37081556+naustica@users.noreply.github.com>
2019-09-25 18:57:26 +0200

    new toggle windows

--- website/src/components/navbar/navbar.tsx
+++ website/src/components/navbar/navbar.tsx
@@ -1,24 +1,24 @@
 import React, {Component} from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { IoMdNotificationsOutline } from 'react-icons/io'
+import { IoMdNotificationsOutline, IoMdSettings, IoMdExit, IoMdVideocam } from 'react-icons/io'
 import { GoDeviceCameraVideo } from 'react-icons/go'
 import { IconContext } from "react-icons"
 
 import './navbar.scss';
 
 
-class Navbar extends Component<{history: any}, {toggle: boolean, term: string, options: any, suggestions: Array<any>, loading: boolean}> {
+class Navbar extends Component<{history: any}, {toggleSettings: boolean, toggleMessage: boolean, term: string, options: any, suggestions: Array<any>, loading: boolean}> {
   constructor(props:any) {
     super(props)
     this.state = {
-      toggle: false,
+      toggleSettings: false,
+      toggleMessage: false,
       term: '',
       options: [],
       suggestions: [],
       loading: false
     }
 
-    this.onClickToggleMenu = this.onClickToggleMenu.bind(this)
     this.onClickUpload = this.onClickUpload.bind(this)
     this.onWindowClick = this.onWindowClick.bind(this)
     this.getTerm = this.getTerm.bind(this)
@@ -103,21 +103,38 @@ class Navbar extends Component<{history: any}, {toggle: boolean, term: string, o
         })
     }
   }
-  onClickToggleMenu(event:any): any {
-    const toggle = this.state.toggle
-    this.setState({toggle: !toggle})
+  onClickToggleNotifications = (event:any): any => {
+    const toggle = this.state.toggleMessage
+    this.setState({toggleMessage: !toggle})
+  }
+  onClickToggleSettings = (event:any): any => {
+    const toggle = this.state.toggleSettings
+    this.setState({toggleSettings: !toggle})
   }
   onClickUpload(event:React.MouseEvent<HTMLButtonElement>): any {
     this.props.history.push('/upload')
   }
   onWindowClick(event):any {
-    if (event.target.className !== 'navbar-user' && event.target.className !== 'navbar-user-pic') {
-      this.setState({toggle: false})
+    if (this.state.toggleSettings) {
+      if (event.target.className !== 'navbar-user' && event.target.className !== 'navbar-user-pic'
+        && event.target.className !== 'navbar-toggle-user-settings')
+          {
+            this.setState({toggleSettings: false})
+          }
+    }
+    if (this.state.toggleMessage) {
+      if (event.target.className !== 'navbar-toggle-message' && event.target.tagName !== 'svg')
+          {
+            this.setState({toggleMessage: false})
+          }
     }
   }
   render() {
-    const toggleStyle = this.state.toggle ? {display: "inline"} : {display: "none"}
+    const toggleSettingsStyle = this.state.toggleSettings ? {display: "inline"} : {display: "none"}
+    const toggleMessageStyle = this.state.toggleMessage ? {display: "inline"} : {display: "none"}
     const searchStyle = this.state.suggestions.length > 0 ? {borderBottomLeftRadius: "0", borderBottomRightRadius: "0"} : {borderBottomLeftRadius: "5px", borderBottomRightRadius: "5px"}
+    const username = localStorage.getItem('username')
+
     return (
       <div className="nav-header">
         <nav className="navbar">
@@ -145,25 +162,57 @@ class Navbar extends Component<{history: any}, {toggle: boolean, term: string, o
             </form>
           </div>
           <div className="navbar-upload">
-            <button type="button" className="upload-button" onClick={this.onClickUpload}>UPLOAD A VIDEO</button>
-          </div>
-          <div className="navbar-messages">
-            <button type="button" className="message-button" name="message">
+            <button type="button" className="upload-button" onClick={this.onClickUpload}>
               <IconContext.Provider value={{size: "26px"}}>
-                <IoMdNotificationsOutline style={{zIndex: 1}}/>
+                <IoMdVideocam />
               </IconContext.Provider>
             </button>
           </div>
-          <div className="navbar-user">
-              <img src="media/default/default_pic_a.jpg" className="navbar-user-pic" onClick={this.onClickToggleMenu} />
+          <div className="navbar-messages">
+            <button type="button" className="message-button" name="message" onClick={this.onClickToggleNotifications}>
+              <IconContext.Provider value={{size: "26px"}}>
+                <IoMdNotificationsOutline />
+              </IconContext.Provider>
+            </button>
+            <div className="navbar-toggle-message" style={toggleMessageStyle}>
+              <div className="navbar-toggle-message-header">
+                Notifications
+              </div>
+              <div className="navbar-toggle-message-body">
+                No new Notifications
+              </div>
+            </div>
           </div>
-        </nav>
-        <div className="toggle-menu" style={toggleStyle}>
-          <ul>
-            <Link to={'/settings'} style={{color: "black"}} onClick={this.onClickToggleMenu}><li>settings</li></Link>
-            <Link to={'/logout'} style={{color: "black"}} onClick={this.onClickToggleMenu}><li>logout</li></Link>
-          </ul>
-      </div>
+          <div className="navbar-user">
+            <img src="media/default/default_pic_a.jpg" className="navbar-user-pic" onClick={this.onClickToggleSettings} />
+            <div className="navbar-toggle-user-settings" style={toggleSettingsStyle}>
+              <div className="navbar-toggle-user-info">
+                <img src="media/default/default_pic_a.jpg" className="navbar-toggle-user-pic" />
+                <div className="navbar-toggle-user-text">
+                  <h1 className="navbar-toggle-user-username">{username}</h1>
+                  <h2 className="navbar-toggle-user-email">test@lul.de</h2>
+                </div>
+              </div>
+              <hr />
+              <Link to={'/settings'} style={{color: "black"}} onClick={this.onClickToggleSettings}>
+                <div className="navbar-toggle-user-settings-links">
+                  <IconContext.Provider value={{size: "25px"}}>
+                    <IoMdSettings style={{color: "#313131", marginRight: "15px"}}/>
+                  </IconContext.Provider>
+                  Settings
+                </div>
+              </Link>
+              <Link to={'/logout'} style={{color: "black"}} onClick={this.onClickToggleSettings}>
+                <div className="navbar-toggle-user-settings-links">
+                  <IconContext.Provider value={{size: "25px"}}>
+                    <IoMdExit style={{color: "#313131", marginRight: "15px"}}/>
+                  </IconContext.Provider>
+                  Logout
+                </div>
+              </Link>
+            </div>
+        </div>
+      </nav>
     </div>
   )
   }
