a991b005547a28a2d2f59d2458d601dc408d074c
Nick <37081556+naustica@users.noreply.github.com>
2019-10-05 13:48:45 +0200

    validate steps with checkmark

--- website/src/components/upload/upload.tsx
+++ website/src/components/upload/upload.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { IoMdCheckmark } from 'react-icons/io'
+import { IconContext } from 'react-icons'
 import cx from 'classnames'
 
 import './upload.scss'
@@ -28,6 +30,7 @@ interface WriteOnly {
   description?: string,
   id?: number,
   step?: number,
+  succeddedSteps?: Array<number>,
   uploadMethod?: string,
   selectedImage?: number,
   originalAuthor?: string,
@@ -36,8 +39,7 @@ interface WriteOnly {
   drag?: boolean
 }
 
-const UPLOAD_TAB_CLASS = 'toggle-upload-header-navbar-steps'
-const FORM_BUTTON_CLASS = 'upload-form-confirm-button'
+const MAX_STEPS: number = 5
 
 const initialState = {
   url: '',
@@ -48,6 +50,7 @@ const initialState = {
   processProgressVideoUpload: 0,
   processProgressThumbnail: 0,
   step: 1,
+  succeddedSteps: [],
   uploadMethod: 'file',
   selectedImage: 1,
   id: 0,
@@ -72,12 +75,30 @@ class Upload extends Component<ReadOnly, WriteOnly> {
   }
 
   private nextStep = (): void => {
-    const { step } = this.state
+    const { step, succeddedSteps } = this.state
+
+    if (this.validateSteps()) {
+      if (!(succeddedSteps.includes(step))) {
+        this.setState({succeddedSteps: [...succeddedSteps, step]})
+      }
+    }
+
     this.setState({step: step + 1})
   }
 
   private prevStep = (): void => {
-    const { step } = this.state
+    const { step, succeddedSteps } = this.state
+
+    if (!this.validateSteps()) {
+      if (succeddedSteps.includes(step)) {
+        for (let i = 0; i < succeddedSteps.length; i++) {
+          if (succeddedSteps[i] === step) {
+            this.setState({succeddedSteps: succeddedSteps.filter(x => x != step)})
+          }
+        }
+      }
+    }
+
     this.setState({step: step - 1, error: ''})
   }
 
@@ -93,8 +114,9 @@ class Upload extends Component<ReadOnly, WriteOnly> {
         if (uploadMethod === '') {
           return false
         }
-        else return true
-
+        else {
+          return true
+        }
       case 2:
         if (!processStatus) {
           return false
@@ -301,8 +323,84 @@ class Upload extends Component<ReadOnly, WriteOnly> {
     this.setState({uploadMethod: method})
   }
 
-  updateSelectedThumbnail = (pictureId: number) => {
+  updateSelectedThumbnail = (pictureId: number): void => {
     this.setState({selectedImage: pictureId})
+  }
+
+  checkSucceddedSteps = (step: number): boolean => {
+    const { succeddedSteps } = this.state
+    if (succeddedSteps.includes(step)) {
+      return true
+    }
+    else false
+  }
+
+  navigateTabs = (step: number): boolean => {
+    const { succeddedSteps } = this.state
+
+    if (succeddedSteps.includes(step) || succeddedSteps.includes(step-1)) {
+      return true
+    }
+    else false
+  }
+
+  selectNavigateTab = (step: number): void => {
+    if (this.navigateTabs(step)) {
+      this.setState({step: step})
+    }
+  }
+
+  renderNavigationTabs = (): any => {
+
+    const { step } = this.state
+
+    const UPLOAD_TAB_CLASS = 'toggle-upload-header-navbar-steps'
+
+    const classNameTab = UPLOAD_TAB_CLASS
+    const selectedClassNameTab = UPLOAD_TAB_CLASS + '--selected'
+    const disabledClassNameTab = UPLOAD_TAB_CLASS + '--disabled'
+
+    let tabs = []
+
+    for (let i = 1; i <= MAX_STEPS; i++) {
+
+      let description: string
+
+      switch (i) {
+        case 1:
+          description = "Upload form"
+          break
+        case 2:
+          description = "Source"
+          break
+        case 3:
+          description = "Basic Info"
+          break
+        case 4:
+          description = "Advanced Settings"
+          break
+        case 5:
+          description = "Publish"
+          break
+      }
+      tabs.push(
+        <span className={cx(classNameTab, {[selectedClassNameTab]: Boolean(step==i)}, {[disabledClassNameTab]: Boolean(!(this.navigateTabs(i)))})} onClick={() => this.selectNavigateTab(i)}>
+          <span className={cx("toggle-upload-header-navbar-steps-number", {["toggle-upload-header-navbar-steps-number--success"]: Boolean(this.checkSucceddedSteps(i))})}>
+            {this.checkSucceddedSteps(i) ?
+              <IconContext.Provider value={{size: "18px"}}>
+                <IoMdCheckmark style={{color: "F5F5F5", paddingBottom: "1px"}}/>
+              </IconContext.Provider>
+              : i
+            }
+          </span>
+          <span className="toggle-upload-header-navbar-steps-description">
+            {description}
+          </span>
+        </span>
+      )
+    }
+
+    return tabs
   }
 
   render() {
@@ -323,9 +421,8 @@ class Upload extends Component<ReadOnly, WriteOnly> {
             hashtags
           } = this.state
 
-    const classNameTab = UPLOAD_TAB_CLASS
-    const selectedClassNameTab = UPLOAD_TAB_CLASS + '--selected'
-    const disabledClassNameTab = UPLOAD_TAB_CLASS + '--disabled'
+    const FORM_BUTTON_CLASS = 'upload-form-confirm-button'
+
     const classNameButton = FORM_BUTTON_CLASS
     const disabledClassNameButton = FORM_BUTTON_CLASS + '--disabled'
 
@@ -335,9 +432,10 @@ class Upload extends Component<ReadOnly, WriteOnly> {
       case 1:
 
         renderStep = <UploadMethod setUploadMethod={this.setUploadMethod} uploadMethod={uploadMethod}/>
-
         break
+
       case 2:
+
         if (uploadMethod === 'link') {
           renderStep = <ProcessLink url={url} updateLink={this.onChange} loading={loading} processLink={this.processLink}/>
         }
@@ -345,17 +443,21 @@ class Upload extends Component<ReadOnly, WriteOnly> {
           renderStep = <ProcessFile dragOverFile={this.dragOverFile} dragLeaveFile={this.dragLeaveFile} dropFile={this.dropFile} onFileSelected={this.onFileSelected} process={processProgressVideoUpload} drag={drag}/>
         }
         break
+
       case 3:
+
         renderStep = <UploadMetadata title={title} description={description} onFileSelected={this.onFileSelected} updateInput={this.onChange} selectedImage={selectedImage} processProgressThumbnail={processProgressThumbnail} updateSelectedThumbnail={this.updateSelectedThumbnail}/>
         break
+
       case 4:
+
         renderStep = <UploadOptionalData originalAuthor={originalAuthor} originalViews={originalViews} hashtags={hashtags} updateInput={this.onChange} />
         break
 
       case 5:
+
         renderStep = <UploadFinish uploadStatus={uploadStatus} validateSteps={this.validateSteps} addInfo={this.addInfo} />
         break
-
     }
     return (
       <div className="toggle-upload-modal" style={this.props.style} ref={this.props.reference}>
@@ -363,46 +465,7 @@ class Upload extends Component<ReadOnly, WriteOnly> {
           <div className="toggle-upload-header">
             <h1>Upload video</h1>
             <div className="toggle-upload-header-navbar">
-              <span className={cx(classNameTab, {[selectedClassNameTab]: Boolean(step==1)}, {[disabledClassNameTab]: Boolean(false)})} onClick={() => this.setState({step: 1})}>
-                <span className="toggle-upload-header-navbar-steps-number">
-                 1
-                </span>
-                <span className="toggle-upload-header-navbar-steps-description">
-                 Upload form
-                </span>
-              </span>
-              <span className={cx(classNameTab, {[selectedClassNameTab]: Boolean(step==2)}, {[disabledClassNameTab]: Boolean(false)})} onClick={() => this.setState({step: 2})}>
-                <span className="toggle-upload-header-navbar-steps-number">
-                  2
-               </span>
-               <span className="toggle-upload-header-navbar-steps-description">
-                Source
-               </span>
-              </span>
-              <span className={cx(classNameTab, {[selectedClassNameTab]: Boolean(step==3)}, {[disabledClassNameTab]: Boolean(false)})} onClick={() => this.setState({step: 3})}>
-                <span className="toggle-upload-header-navbar-steps-number">
-                  3
-                </span>
-                <span className="toggle-upload-header-navbar-steps-description">
-                 Basic Info
-                </span>
-              </span>
-              <span className={cx(classNameTab, {[selectedClassNameTab]: Boolean(step==4)}, {[disabledClassNameTab]: Boolean(false)})} onClick={() => this.setState({step: 4})}>
-                <span className="toggle-upload-header-navbar-steps-number">
-                  4
-                </span>
-                <span className="toggle-upload-header-navbar-steps-description">
-                 Advanced Settings
-                </span>
-              </span>
-              <span className={cx(classNameTab, {[selectedClassNameTab]: Boolean(step==5)}, {[disabledClassNameTab]: Boolean(false)})} onClick={() => this.setState({step: 5})}>
-                <span className="toggle-upload-header-navbar-steps-number">
-                  5
-                </span>
-                <span className="toggle-upload-header-navbar-steps-description">
-                 Publish
-                </span>
-              </span>
+              {this.renderNavigationTabs()}
             </div>
           </div>
           <hr />
