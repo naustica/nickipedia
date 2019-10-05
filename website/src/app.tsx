c0ac833eb9e9b4f624da055deaf231bcb5ef6bb0
Nick <37081556+naustica@users.noreply.github.com>
2019-10-05 18:28:37 +0200

    delete some old routes

--- website/src/app.tsx
+++ website/src/app.tsx
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Provider} from 'react-redux';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-import './app.scss';
-import store from './store/store';
+import './app.scss'
+import store from './store/store'
 
-import Navbar from './components/navbar/navbar';
-import Home from './components/home/home';
-import Login from './components/login/login';
-import Register from './components/login/register';
-import Results from './components/results/results';
-import Logout from './components/logout/logout';
-import Video from './components/video/video';
-//import Footer from './components/footer/footer';
-//import Upload from './components/upload/upload';
-import Settings from './components/settings/settings';
-import AuthenticatedRoute from './components/authentication/authentication';
+import Navbar from './components/navbar/navbar'
+import Home from './components/home/home'
+import Login from './components/login/login'
+import Results from './components/results/results'
+import Logout from './components/logout/logout'
+import Video from './components/video/video'
+//import Footer from './components/footer/footer'
+import Settings from './components/settings/settings'
+import AuthenticatedRoute from './components/authentication/authentication'
 
-
-function About() {
-  return <h2>about</h2>
-}
 
 function Error() {
   return <h2>error</h2>
@@ -32,15 +26,13 @@ class App extends Component {
     render() {
       return (
           <Router>
-            <Route path={['/', '/home', '/settings', '/about', '/result/:term', '/watch/:id', '/upload']} exact component={Navbar} />
+            <Route path={['/', '/home', '/settings', '/result/:term', '/watch/:id']} exact component={Navbar} />
               <div className="app">
                   <Switch>
                     <AuthenticatedRoute path='/' exact component={Home}/>
                     <Route path='/home' exact component={Home}/>
                     <Route path='/settings' component={Settings}/>
-                    <Route path='/about' component={About}/>
                     <Route path='/login' exact component={Login}/>
-                    <Route path='/register' exact component={Register}/>
                     <AuthenticatedRoute path='/logout' exact component={Logout}/>
                     <Route path='/result/:term' component={Results}/>
                     <Route path='/watch/:id' component={Video}/>
@@ -54,4 +46,4 @@ class App extends Component {
 }
 
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
