a775501dfa4682f855188176c4f18ac76e3e1319
Nick <37081556+naustica@users.noreply.github.com>
2019-10-07 21:13:42 +0200

    add arrow key codes

--- website/src/components/video/stream/stream.tsx
+++ website/src/components/video/stream/stream.tsx
@@ -98,6 +98,14 @@ class VideoStream extends Component<ReadOnly, WriteOnly> {
     if (event.keyCode === 40) {
       event.preventDefault()
     }
+    // arrow left
+    if (event.keyCode === 37) {
+      event.preventDefault()
+    }
+    // arrow right
+    if (event.keyCode === 39) {
+      event.preventDefault()
+    }
   }
   toggleMuted = () => {
     this.setState({muted: !this.state.muted})
